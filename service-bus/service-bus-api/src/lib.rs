37dc02820fb46ffdc82733822b5788169456c6cc
Przemyslaw Rekucki <prekucki@rcl.pl>
2019-11-21 10:43:09 +0100

    sb mock

--- service-bus/service-bus-api/src/lib.rs
+++ service-bus/service-bus-api/src/lib.rs
@@ -1,16 +1,49 @@
-use serde::{Serialize, de::DeserializeOwned};
+use serde::{de::DeserializeOwned, Serialize};
+use std::future::Future;
 
 pub struct BusPath(Vec<String>);
 
 impl From<&[&str]> for BusPath {
-
     fn from(path: &[&str]) -> Self {
         BusPath(path.into_iter().map(|&s| s.into()).collect())
     }
-
 }
 
-pub trait RpcMessage : Clone + Serialize + DeserializeOwned + 'static + Sync + Send {
-    const ID : &'static str;
+pub trait BusMessage: Clone + Serialize + DeserializeOwned + 'static + Sync + Send {}
+
+pub trait RpcMessage: BusMessage {
+    const ID: &'static str;
+    type Reply: BusMessage;
 }
 
+pub trait RpcEndpoint<T: RpcMessage> {
+    type Result: Future<Output = T::Reply>;
+
+    fn send(&self, msg: T) -> Self::Result;
+}
+
+pub trait RpcHandler<T: RpcMessage> {
+    type Result: Future<Output = T::Reply>;
+
+    fn handle(&mut self, caller: BusPath, msg: T) -> Self::Result;
+}
+
+pub struct Handle;
+
+pub fn bind<T: RpcMessage>(addr: &BusPath, endpoint: impl RpcHandler<T>) -> Handle {
+    unimplemented!()
+}
+
+pub fn service<T: RpcMessage>(addr: &BusPath) -> impl RpcEndpoint<T> {
+    MockEndpoint(unimplemented!())
+}
+
+struct MockEndpoint<T>(T);
+
+impl<M : RpcMessage> RpcEndpoint<M> for MockEndpoint<M> where {
+    type Result = futures::future::Ready<M::Reply>;
+
+    fn send(&self, msg: M) -> Self::Result {
+        unimplemented!()
+    }
+}
