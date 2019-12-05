ed124d74c04639ebdd53b38eca4c32f8f2ffb348
Marek Franciszkiewicz <marek@golem.network>
2019-12-05 14:42:14 +0100

    Activity API models

--- interfaces/model/src/activity.rs
+++ interfaces/model/src/activity.rs
@@ -1,1 +1,21 @@
+pub mod activity_state;
+pub mod activity_usage;
+pub mod error_message;
+pub mod exe_script_batch;
+pub mod exe_script_command;
+pub mod exe_script_command_result;
+pub mod exe_script_command_state;
+pub mod exe_script_request;
+pub mod problem_details;
+pub mod provider_event;
 
+pub use self::activity_state::ActivityState;
+pub use self::activity_usage::ActivityUsage;
+pub use self::error_message::ErrorMessage;
+pub use self::exe_script_batch::ExeScriptBatch;
+pub use self::exe_script_command::ExeScriptCommand;
+pub use self::exe_script_command_result::ExeScriptCommandResult;
+pub use self::exe_script_command_state::ExeScriptCommandState;
+pub use self::exe_script_request::ExeScriptRequest;
+pub use self::problem_details::ProblemDetails;
+pub use self::provider_event::{gsb, ProviderEvent};
