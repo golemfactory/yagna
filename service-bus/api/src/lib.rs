8452da50a0a69879e97142ac0e6c400d599404f3
Adam Wierzbicki <awierzbicki@golem.network>
2019-12-09 16:15:31 +0100

    Service bus API - new header type
    Protobuf doesn't support fixed-length messages so a different approach
    was needed to have a proper message header.
    
    Signed-off-by: Adam Wierzbicki <awierzbicki@golem.network>

--- service-bus/api/src/lib.rs
+++ service-bus/api/src/lib.rs
@@ -2,4 +2,50 @@ mod gsb_api {
     include!(concat!(env!("OUT_DIR"), "/gsb_api.rs"));
 }
 
+use bytes::BytesMut;
+use failure;
+use std::mem::size_of;
+
 pub use gsb_api::*;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum MessageType {
+    RegisterRequest = 0,
+    RegisterReply = 1,
+    UnregisterRequest = 2,
+    UnregisterReply = 3,
+    CallRequest = 4,
+    CallReply = 5,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct MessageHeader {
+    pub msg_type: i32,
+    pub msg_length: u32,
+}
+
+impl MessageHeader {
+    pub fn encode(&self, buf: &mut BytesMut) {
+        buf.extend_from_slice(&self.msg_type.to_be_bytes());
+        buf.extend_from_slice(&self.msg_length.to_be_bytes());
+    }
+
+    pub fn decode(mut src: BytesMut) -> failure::Fallible<Self> {
+        if src.len() < size_of::<MessageHeader>() {
+            return Err(failure::err_msg(
+                "Cannot decode message header: not enough bytes",
+            ));
+        }
+
+        let mut msg_type: [u8; 4] = [0; 4];
+        let mut msg_length: [u8; 4] = [0; 4];
+        msg_type.copy_from_slice(src.split_to(size_of::<i32>()).as_ref());
+        msg_length.copy_from_slice(src.split_to(size_of::<u32>()).as_ref());
+
+        Ok(MessageHeader {
+            msg_type: i32::from_be_bytes(msg_type),
+            msg_length: u32::from_be_bytes(msg_length),
+        })
+    }
+}
