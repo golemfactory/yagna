17f7aef10bcbde95d135bb2a1b2a4199fe1d0d11
Adam Wierzbicki <awierzbicki@golem.network>
2019-12-09 16:16:11 +0100

    Service bus router
    First working implementation of the GSB router.
    
    Signed-off-by: Adam Wierzbicki <awierzbicki@golem.network>

--- service-bus/router/src/lib.rs
+++ service-bus/router/src/lib.rs
@@ -1,4 +1,30 @@
 #[macro_use]
 extern crate lazy_static;
 
-pub mod decoder;
+pub mod codec;
+
+use std::net::SocketAddr;
+
+use futures::compat::Future01CompatExt;
+
+use tokio::codec::{FramedRead, FramedWrite};
+use tokio::io::{AsyncRead, ReadHalf, WriteHalf};
+use tokio::net::TcpStream;
+
+use crate::codec::{GsbMessageDecoder, GsbMessageEncoder};
+
+pub async fn connect(
+    addr: &SocketAddr,
+) -> (
+    FramedRead<ReadHalf<TcpStream>, GsbMessageDecoder>,
+    FramedWrite<WriteHalf<TcpStream>, GsbMessageEncoder>,
+) {
+    let sock = TcpStream::connect(&addr)
+        .compat()
+        .await
+        .expect("Connect failed");
+    let (reader, writer) = sock.split();
+    let reader = FramedRead::new(reader, GsbMessageDecoder::new());
+    let writer = FramedWrite::new(writer, GsbMessageEncoder {});
+    (reader, writer)
+}
