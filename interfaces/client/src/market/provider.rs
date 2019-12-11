59be638a9ecbf7d1d0aa0687f623498f227d0fe6
Piotr Chromiec <tworec@golem.network>
2019-12-11 09:58:42 +0100

    rest macro first working ver

--- interfaces/client/src/market/provider.rs
+++ interfaces/client/src/market/provider.rs
@@ -6,100 +6,112 @@ use super::ApiConfiguration;
 use crate::Result;
 use ya_model::market::{AgreementProposal, Offer, Proposal, ProviderEvent};
 
-pub struct ProviderApi {
-    configuration: Arc<ApiConfiguration>,
-}
+//pub struct ProviderApi {
+//    configuration: Arc<ApiConfiguration>,
+//}
+//
+//impl ProviderApi {
+//    pub fn new(configuration: Arc<ApiConfiguration>) -> Self {
+//        ProviderApi { configuration }
+//    }
+//
+//    /// Publish Provider’s service capabilities (Offer) on the market to declare an
+//    /// interest in Demands meeting specified criteria.
+//    pub async fn subscribe(&self, offer: Offer) -> Result<String> {
+//        let result = Client::default()
+//            .post(self.configuration.api_endpoint("offers"))
+//            .send_json(&offer)
+//            .compat()
+//            .await?
+//            .body()
+//            .compat()
+//            .await?;
+//        let vec = result
+//            .to_vec();
+//        Ok(String::from_utf8(vec)?)
+//    }
+//}
 
-impl ProviderApi {
-    pub fn new(configuration: Arc<ApiConfiguration>) -> Self {
-        ProviderApi { configuration }
-    }
+rest_interface! {
+    pub ProviderApi {
+        #[field] configuration: Arc<ApiConfiguration>;
 
-    /// Publish Provider’s service capabilities (Offer) on the market to declare an
-    /// interest in Demands meeting specified criteria.
-    pub async fn subscribe(&self, offer: Offer) -> Result<String> {
-        let endpoint_url = self.configuration.api_endpoint("offers");
-//        async move {
-            let vec = Client::default()
-                .post(endpoint_url)
-                .send_json(&offer)
-                .compat()
-                .await?
-                .body()
-                .compat()
-                .await?
-                .to_vec();
-            Ok(String::from_utf8(vec)?)
-//        }
-    }
-
-    /// Stop subscription by invalidating a previously published Offer.
-    pub fn unsubscribe(&self, subscription_id: &str) -> impl Future<Output = Result<()>> {
-        //        Box::pin(async {
-        //            Client::default()
-        //                .delete(self.configuration.api_endpoint(format!("/offers/{}", subscription_id))?)
-        //                .send_json(&Offer::new(serde_json::json!({"zima":"już"}), "()".into()))
-        //                .await
-        //                .expect("Offers POST request failed")
-        //        })
-        async { unimplemented!() }
-    }
-
-    /// Get events which have arrived from the market in response to the Offer
-    /// published by the Provider via  [subscribe](self::subscribe).
-    /// Returns collection of [ProviderEvents](ProviderEvent) or timeout.
-    pub fn collect(
-        &self,
-        subscription_id: &str,
-        timeout: f32,
-        max_events: i64,
-    ) -> impl Future<Output = Result<Vec<ProviderEvent>>> {
-        //            "/offers/{subscriptionId}/events",
-        async { unimplemented!() }
-    }
-
-    /// TODO doc
-    pub fn create_proposal(
-        &self,
-        subscription_id: &str,
-        proposal_id: &str,
-        proposal: Proposal,
-    ) -> impl Future<Output = Result<String>> {
-        //            "/offers/{subscriptionId}/proposals/{proposalId}/offer".to_string(),
-        async { unimplemented!() }
-    }
-
-    /// TODO doc
-    pub fn get_proposal(
-        &self,
-        subscription_id: &str,
-        proposal_id: &str,
-    ) -> impl Future<Output = Result<AgreementProposal>> {
-        //            "/offers/{subscriptionId}/proposals/{proposalId}".to_string(),
-        async { unimplemented!() }
-    }
-
-    /// TODO doc
-    pub fn reject_proposal(
-        &self,
-        subscription_id: &str,
-        proposal_id: &str,
-    ) -> impl Future<Output = Result<()>> {
-        //            "/offers/{subscriptionId}/proposals/{proposalId}".to_string(),
-        async { unimplemented!() }
-    }
-
-    /// Confirms the Agreement received from the Requestor.
-    /// Mutually exclusive with [reject_agreement](self::reject_agreement).
-    pub fn approve_agreement(&self, agreement_id: &str) -> impl Future<Output = Result<()>> {
-        //            "/agreements/{agreementId}/approve".to_string(),
-        async { unimplemented!() }
-    }
-
-    /// Rejects the Agreement received from the Requestor.
-    /// Mutually exclusive with [approve_agreement](self::approve_agreement).
-    pub fn reject_agreement(&self, agreement_id: &str) -> impl Future<Output = Result<()>> {
-        //            "/agreements/{agreementId}/reject".to_string(),
-        async { unimplemented!() }
+        #[REST(post, "offers", body)]
+        fn subscribe(&self, offer: Offer) -> Result<String> {
+            #[result] result;
+            { Ok(String::from_utf8(result.to_vec())?) }
+        }
     }
 }
+
+
+//    /// Stop subscription by invalidating a previously published Offer.
+//    pub fn unsubscribe(&self, subscription_id: &str) -> impl Future<Output = Result<()>> {
+//        //        Box::pin(async {
+//        //            Client::default()
+//        //                .delete(self.configuration.api_endpoint(format!("/offers/{}", subscription_id))?)
+//        //                .send_json(&Offer::new(serde_json::json!({"zima":"już"}), "()".into()))
+//        //                .await
+//        //                .expect("Offers POST request failed")
+//        //        })
+//        async { unimplemented!() }
+//    }
+//
+//    /// Get events which have arrived from the market in response to the Offer
+//    /// published by the Provider via  [subscribe](self::subscribe).
+//    /// Returns collection of [ProviderEvents](ProviderEvent) or timeout.
+//    pub fn collect(
+//        &self,
+//        subscription_id: &str,
+//        timeout: f32,
+//        max_events: i64,
+//    ) -> impl Future<Output = Result<Vec<ProviderEvent>>> {
+//        //            "/offers/{subscriptionId}/events",
+//        async { unimplemented!() }
+//    }
+//
+//    /// TODO doc
+//    pub fn create_proposal(
+//        &self,
+//        subscription_id: &str,
+//        proposal_id: &str,
+//        proposal: Proposal,
+//    ) -> impl Future<Output = Result<String>> {
+//        //            "/offers/{subscriptionId}/proposals/{proposalId}/offer"
+//        async { unimplemented!() }
+//    }
+//
+//    /// TODO doc
+//    pub fn get_proposal(
+//        &self,
+//        subscription_id: &str,
+//        proposal_id: &str,
+//    ) -> impl Future<Output = Result<AgreementProposal>> {
+//        //            "/offers/{subscriptionId}/proposals/{proposalId}"
+//        async { unimplemented!() }
+//    }
+//
+//    /// TODO doc
+//    pub fn reject_proposal(
+//        &self,
+//        subscription_id: &str,
+//        proposal_id: &str,
+//    ) -> impl Future<Output = Result<()>> {
+//        //            "/offers/{subscriptionId}/proposals/{proposalId}"
+//        async { unimplemented!() }
+//    }
+//
+//    /// Confirms the Agreement received from the Requestor.
+//    /// Mutually exclusive with [reject_agreement](self::reject_agreement).
+//    pub fn approve_agreement(&self, agreement_id: &str) -> impl Future<Output = Result<()>> {
+//        //            "/agreements/{agreementId}/approve"
+//        async { unimplemented!() }
+//    }
+//
+//    /// Rejects the Agreement received from the Requestor.
+//    /// Mutually exclusive with [approve_agreement](self::approve_agreement).
+//    pub fn reject_agreement(&self, agreement_id: &str) -> impl Future<Output = Result<()>> {
+//        //            "/agreements/{agreementId}/reject"
+//        async { unimplemented!() }
+//    }
+//}
--- interfaces/client/src/rest.rs
+++ interfaces/client/src/rest.rs
@@ -1,37 +1,66 @@
-//#[macro_export]
-//macro_rules! rest_interface {
-//    {
-//    pub $interface_name:ident {
-//        $(
-//            $(#[doc = $doc:expr])*
-//            $field:ident $type:ty;
-//        )*
-//
-//        $(
-//            $(#[doc = $doc:expr])*
-//            #[$http_method($rest_uri:expr, $response_extractor:ident)]
-//            fn $it:tt $args:tt -> Result<$ret:ty>;
-//        )*
-//    }
-//
-//    $(
-//        converter $converter_name:ident $converter_method:ident;
-//    )?
-//    }
-//    => {
-//        pub struct ProviderApi {
-//        configuration: Arc<ApiConfiguration>,
-//        }
-//
-//        impl ProviderApi {
-//            pub fn new(configuration: Arc<ApiConfiguration>) -> Self {
-//                ProviderApi { configuration }
-//            }
-//
-//            pub fn new(configuration: Arc<ApiConfiguration>) -> Self {
-//                $interface_name { configuration }
-//            }
-//        }
-//
-//    }
-//}
+#[macro_export]
+macro_rules! rest_interface {
+    {
+//        $(#[doc = $interface_doc:expr])*
+        pub $interface_name:ident {
+            $(
+//                $(#[doc = $field_doc:expr])*
+                #[field]
+                $field:ident : $type:ty;
+            )*
+            
+            $(
+//                $(#[doc = $api_doc:expr])*
+                #[ REST ( $http_method:ident, $rest_uri:expr, $response_extractor:ident ) ]
+                fn $fn_name:ident (&self, $( $arg:ident : $arg_type:ty )* ) -> Result<$ret:ty> {
+                    #[result] $result:ident;
+                    $body:tt
+                }
+            )+
+        }
+    }
+    => {
+//        $(#[doc = $interface_doc])*
+        pub struct $interface_name {
+            $(
+//                $(#[doc = $field_doc])*
+                $field: $type,
+            )*
+        }
+
+        impl $interface_name {
+            pub fn new(
+                $(
+                    $field: $type,
+                )*
+            ) -> Self {
+                $interface_name {
+                    $(
+                        $field,
+                    )*
+                }
+            }
+
+            $(
+//                $(#[doc = $api_doc])*
+//                #[doc = "Does `"]
+//                #[doc = $http_method]
+//                #[doc = "` on `"]
+//                #[doc = $rest_uri]
+//                #[doc = "` REST URI."]
+                pub async fn $fn_name (&self, $( $arg : $arg_type )* ) -> Result<$ret> {
+                    let $result = Client::default()
+                        .$http_method(self.configuration.api_endpoint($rest_uri))
+                        .send_json( $( &$arg )* )
+                        .compat()
+                        .await?
+                        .$response_extractor()
+                        .compat()
+                        .await?;
+                    $body
+                }
+            )+
+        }
+
+    }
+}
