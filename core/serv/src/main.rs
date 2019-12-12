49f06d803d44fca08cbb253e4bca80608c1350c7
Piotr Chromiec <tworec@golem.network>
2019-12-12 18:11:53 +0100

    Yagna daemon foundation; resolves LWG-41

--- core/serv/src/main.rs
+++ core/serv/src/main.rs
@@ -1,11 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, middleware, App, HttpServer, Responder};
+use std::fmt::Debug;
+use std::path::PathBuf;
+use structopt::*;
+
+#[derive(StructOpt, Debug)]
+#[structopt(global_setting = clap::AppSettings::ColoredHelp)]
+#[structopt(about = clap::crate_description!())]
+struct CliArgs {
+    #[cfg(feature = "interactive_cli")]
+    /// Enter interactive mode
+    #[structopt(short, long)]
+    interactive: bool,
+
+    /// Yagna daemon address
+    #[structopt(short, long)]
+    #[structopt(display_order = 500)]
+    #[structopt(set = clap::ArgSettings::Global)]
+    address: Option<String>,
+
+    /// Yagna daemon port
+    #[structopt(short, long)]
+    #[structopt(display_order = 500)]
+    #[structopt(set = clap::ArgSettings::Global)]
+    port: Option<u16>,
+
+    /// Yagna daemon data dir
+    #[structopt(short, long = "datadir")]
+    #[structopt(set = clap::ArgSettings::Global)]
+    data_dir: Option<PathBuf>,
+
+    /// Return results in JSON format
+    #[structopt(long)]
+    #[structopt(display_order = 500)]
+    #[structopt(set = clap::ArgSettings::Global)]
+    json: bool,
+    //    #[structopt(subcommand)]
+    //    command: Option<commands::CommandSection>,
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+impl CliArgs {
+    #[allow(dead_code)]
+    pub fn get_data_dir(&self) -> PathBuf {
+        match &self.data_dir {
+            Some(data_dir) => data_dir.to_owned(),
+            None => appdirs::user_data_dir(Some("yagna"), Some("golem"), false)
+                .unwrap()
+                .join("default"),
+        }
     }
+
+    pub fn get_address(&self) -> failure::Fallible<(&str, u16)> {
+        let address = match &self.address {
+            Some(a) => a.as_str(),
+            None => "127.0.0.1",
+        };
+
+        Ok((address.into(), self.port.unwrap_or(7465)))
+    }
+}
+
+#[get("/")]
+fn index() -> impl Responder {
+    format!("Hello {}!", clap::crate_description!())
+}
+
+fn main() -> failure::Fallible<()> {
+    let args = CliArgs::from_args();
+
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    flexi_logger::Logger::with_env_or_str("error")
+        .start()
+        .unwrap();
+
+    println!("Hello {}!", clap::crate_description!());
+
+    Ok(HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(index)
+    })
+    .bind(args.get_address()?)?
+    .run()?)
 }
