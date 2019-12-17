7d7917aa9250369d29b9b4fba3995f51d92ad9f4
Filip <filip@golem.network>
2019-12-17 12:18:46 +0100

    Fix examples

--- core/net/src/lib.rs
+++ core/net/src/lib.rs
@@ -4,30 +4,28 @@ use futures03::compat::Future01CompatExt;
 use ya_core_model::net::{GetMessages, Message, SendMessage, SendMessageError};
 use ya_service_bus::typed as bus;
 
-pub const HUB_URL: &str = "localhost:8080";
+pub const HUB_URL: &str = "http://localhost:8080";
 
 pub const SERVICE_ID: &str = "/local/net";
 
 pub fn init_service() {
     let _ = bus::bind(SERVICE_ID, |command: SendMessage| {
         Client::default()
-            .post(HUB_URL)
-            .send_json(&command)
+            .post(format!("{}/message", HUB_URL))
+            .send_json(&command.message)
             .map_err(|e| SendMessageError(e.to_string()))
             .and_then(|_| Ok(()))
             .compat()
     });
     let _ = bus::bind(SERVICE_ID, |command: GetMessages| {
         Client::default()
-            .get(format!("{}/{}", HUB_URL, command.0))
+            .get(format!("{}/message/{}", HUB_URL, command.0))
             .send()
             .map_err(|e| SendMessageError(e.to_string()))
             .and_then(|mut x| x.json().map_err(|e| SendMessageError(e.to_string())))
             .and_then(|x: Vec<Message>| Ok(x))
             .compat()
     });
-    //bus::service(SERVICE_ID).send(SendMessage(...));
-    //bus::service(SERVICE_ID).send(GetMessages("0x123".into()));
 }
 
 #[cfg(test)]
