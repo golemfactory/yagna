691bbacedb44c7a9d932d91f8235ae6833d24813
Marek Franciszkiewicz <marek@golem.network>
2020-01-10 18:26:51 +0100

    Embed core::appkey migrations Update the example

--- core/appkey/src/lib.rs
+++ core/appkey/src/lib.rs
@@ -1,8 +1,16 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
 
 pub mod cli;
 pub(crate) mod dao;
 pub(crate) mod db;
 pub mod error;
 pub mod service;
+
+#[allow(dead_code)]
+pub mod migrations {
+    #[derive(EmbedMigrations)]
+    struct _Dummy;
+}
