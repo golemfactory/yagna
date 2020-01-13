459d99b444b9c08cd8aefe61c832cff74f2a4c62
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-01-13 17:44:16 +0100

    Add Negotiator api

--- agent/provider/src/market/mock_negotiator.rs
+++ agent/provider/src/market/mock_negotiator.rs
@@ -1,4 +1,7 @@
 use super::negotiator::{Negotiator};
+use ya_model::market::{Demand, Offer, AgreementProposal};
+use crate::market::negotiator::{ProposalResponse, AgreementResponse};
+use ya_client::Error;
 
 
 pub struct AcceptAllNegotiator {
@@ -8,6 +11,17 @@ pub struct AcceptAllNegotiator {
 
 impl Negotiator for AcceptAllNegotiator {
 
+    fn create_offer(&self) -> Result<Offer, Error> {
+        unimplemented!()
+    }
+
+    fn react_to_proposal(&self, _demand: &Demand) -> Result<ProposalResponse, Error> {
+        Ok(ProposalResponse::RejectProposal)
+    }
+
+    fn react_to_agreement(&self, _proposal: &AgreementProposal) -> Result<AgreementResponse, Error> {
+        Ok(AgreementResponse::AcceptAgreement)
+    }
 }
 
 impl AcceptAllNegotiator {
--- agent/provider/src/market/negotiator.rs
+++ agent/provider/src/market/negotiator.rs
@@ -1,10 +1,30 @@
-use ya_model::market::{Offer,};
+use ya_model::market::{Offer, Demand, AgreementProposal, Proposal};
 use ya_client::{Result,};
 
 
 
+/// Response for requestor proposals.
+pub enum ProposalResponse {
+    CounterProposal {
+        proposal: Proposal
+    },
+    RejectProposal,
+    ///< Don't send any message to requestor. Could be useful to wait for other offers.
+    IgnoreProposal,
+}
+
+/// Response for requestor agreements.
+pub enum AgreementResponse {
+    AcceptAgreement,
+    RejectAgreement,
+}
+
 
 pub trait Negotiator {
 
-    //fn create_offer(&self) -> Result< Offer >;
+    //TODO: We should add some parameters for offer creation.
+    fn create_offer(&self) -> Result< Offer >;
+
+    fn react_to_proposal(&self, demand: &Demand) -> Result<ProposalResponse>;
+    fn react_to_agreement(&self, proposal: &AgreementProposal) -> Result<AgreementResponse>;
 }
