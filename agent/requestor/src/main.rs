6b66165ee4b8e4c28d11adde75535cc6650eb0f0
Przemysław K. Rekucki <przemyslaw.rekucki@golem.network>
2020-01-17 14:21:31 +0100

    WIP: requestor mock

--- agent/requestor/src/main.rs
+++ agent/requestor/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use ya_client::{
+    market::{ApiClient, ProviderApi, RequestorApi},
+    web::WebClient,
+    Error, Result,
+};
+use ya_model::market::{Agreement, Demand, Offer, Proposal, ProviderEvent, RequestorEvent};
+
+#[actix_rt::main]
+async fn main() -> anyhow::Result<()> {
+    let client = ApiClient::new(WebClient::builder())?;
+
+    let demand = Demand {
+        properties: json!{{
+            "golem": {
+                "node": {
+                    "id": ""
+                }
+            }
+        }},
+        constraints: r#"
+
+
+
+        "#.to_string()
+    };
+
+    let subscription_id = client.requestor().subscribe(&demand).await?;
+
+    eprintln!("sub_id={}", subscription_id);
+
+    Ok(())
 }
