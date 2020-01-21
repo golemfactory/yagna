48595204f6c45554dfc361dccc82c086ad121914
Marek Franciszkiewicz <marek@golem.network>
2020-01-21 17:50:55 +0100

    Return 400 in create_activity (requestor) if agreement does not exist

--- core/activity/src/requestor/control.rs
+++ core/activity/src/requestor/control.rs
@@ -36,7 +36,10 @@ async fn create_activity(
     let agreement = AgreementDao::new(&conn)
         .get(&agreement_id)
         .not_found_as_option()?
-        .ok_or(Error::NotFound)?;
+        .ok_or(Error::BadRequest(format!(
+            "Unknown agreement id: {}",
+            body.agreement_id
+        )))?;
     log::debug!("agreement: {:?}", agreement);
 
     let uri = provider_activity_uri(&agreement.offer_node_id);
