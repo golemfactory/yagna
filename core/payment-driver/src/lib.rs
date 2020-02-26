144e090afd65ec53ae93130d817a5c15467cdb28
Adam Wierzbicki <awierzbicki@golem.network>
2020-02-26 17:23:12 +0100

    New type for transaction signing function
    The reason for this change is to get rid of template methods from
    PaymentDriver. Template methods prevented making this trait into an object.
    
    Signed-off-by: Adam Wierzbicki <awierzbicki@golem.network>

--- core/payment-driver/src/lib.rs
+++ core/payment-driver/src/lib.rs
@@ -24,22 +24,22 @@ pub use payment::{PaymentAmount, PaymentConfirmation, PaymentDetails, PaymentSta
 
 pub type PaymentDriverResult<T> = Result<T, PaymentDriverError>;
 
+pub type SignTx<'a> = &'a (dyn Fn(Vec<u8>) -> Vec<u8> + Send + Sync);
+
 #[async_trait]
 pub trait PaymentDriver {
     /// Returns account balance
     async fn get_account_balance(&self) -> PaymentDriverResult<AccountBalance>;
 
     /// Schedules payment
-    async fn schedule_payment<F>(
+    async fn schedule_payment(
         &mut self,
         invoice_id: &str,
         amount: PaymentAmount,
         recipient: Address,
         due_date: DateTime<Utc>,
-        sign_tx: F,
-    ) -> PaymentDriverResult<()>
-    where
-        F: 'static + FnOnce(Vec<u8>) -> Vec<u8> + Sync + Send;
+        sign_tx: SignTx<'_>,
+    ) -> PaymentDriverResult<()>;
 
     /// Returns payment status
     async fn get_payment_status(&self, invoice_id: &str) -> PaymentDriverResult<PaymentStatus>;
