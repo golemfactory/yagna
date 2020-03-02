fa69a3c2486c1a12158f9abc6dee389932fa6eca
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-03-02 14:54:35 +0100

    Publish mutliple files

--- core/gftp/readme.md
+++ core/gftp/readme.md
@@ -9,7 +9,7 @@ cargo run --bin yagna service run
 
 Publish chosen file. Copy file hash from logs.
 ```
-cargo run --bin gftp -- publish -f {file name}
+cargo run --bin gftp -- publish {file name}
 ...
 Published file [LICENSE] as gftp://0x06bf342e4d1633aac5db38817c2e938e9d6ab7f3/edb0016d9f8bafb54540da34f05a8d510de8114488f23916276bdead05509a53.
 ...
--- core/gftp/src/bin/gftp.rs
+++ core/gftp/src/bin/gftp.rs
@@ -7,8 +7,7 @@ use url::Url;
 #[derive(StructOpt)]
 pub enum CmdLine {
     Publish {
-        #[structopt(short = "f", long = "file", help = "File to publish")]
-        path: PathBuf,
+        files: Vec<PathBuf>,
     },
     Download {
         url: Url,
@@ -24,10 +23,11 @@ async fn main() -> Result<()> {
     let cmd_args = CmdLine::from_args();
 
     match cmd_args {
-        CmdLine::Publish { path } => {
-            let url = gftp::publish(&path).await?;
-
-            info!("Published file [{}] as {}.", &path.display(), url,);
+        CmdLine::Publish { files } => {
+            for path in files {
+                let url = gftp::publish(&path).await?;
+                info!("Published file [{}] as {}.", &path.display(), url,);
+            }
 
             actix_rt::signal::ctrl_c().await?;
             info!("Received ctrl-c signal. Shutting down.")
