0abf4292f3dd0e9002d34827593442ac56d6dbe0
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-03-03 14:19:44 +0100

    Fix ambigous access warning in lib.rs

--- core/gftp/src/lib.rs
+++ core/gftp/src/lib.rs
@@ -1,3 +1,3 @@
 mod gftp;
 
-pub use gftp::{download_file, download_from_url, open_for_upload, publish, upload_file};
+pub use self::gftp::{download_file, download_from_url, open_for_upload, publish, upload_file};
