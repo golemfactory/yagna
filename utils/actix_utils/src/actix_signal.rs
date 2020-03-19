f82fa6626e7b6bbf3a84dd13c8004fe8dddd0562
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-03-19 15:36:00 +0100

    send_signal returns Result

--- utils/actix_utils/src/actix_signal.rs
+++ utils/actix_utils/src/actix_signal.rs
@@ -1,6 +1,6 @@
 use actix::prelude::*;
-use anyhow::Result;
-use log::error;
+use anyhow::{Result, anyhow};
+use crate::actix_handler::ResultTypeGetter;
 
 /// Subscribe to process signals.
 #[derive(Message)]
@@ -33,16 +33,26 @@ where
 
     /// Send signal to all subscribers
     pub fn send_signal(&self, message: MessageType) -> Result<()> {
-        for subscriber in &self.subscribers {
-            if let Err(error) = subscriber.do_send(message.clone()) {
-                //TODO: It would be useful to have better error message, that suggest which signal failed.
-                error!(
-                    "Sending signal to subscriber failed in SignalSlot::send_signal. {}",
-                    error
-                );
-            }
+        let errors = self.subscribers
+            .iter()
+            .map(|subscriber|{ subscriber.do_send(message.clone()) })
+            .filter_map(|result| {
+                match result {
+                    Err(error) => {
+                        //TODO: It would be useful to have better error message, that suggest which signal failed.
+                        log::error!("Sending signal to subscriber failed in SignalSlot::send_signal. {}", error);
+                        Some(error)
+                    },
+                    Ok(_) => None
+                }
+            })
+            .collect::<Vec<SendError<MessageType>>>();
+
+        if errors.is_empty() {
+            Ok(())
+        } else {
+            Err(anyhow!("Errors while sending signal: {:?}", errors))
         }
-        Ok(())
     }
 
     pub fn subscribe(&mut self, subscriber: Recipient<MessageType>) {
