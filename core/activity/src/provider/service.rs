4208d625da9343996861b333fdba58b754d70c02
Piotr Chromiec <tworec@golem.network>
2020-03-27 03:39:17 +0100

    [bus,activity,payment] ServiceBinder generalized and moved to typed bus

--- core/activity/src/provider/service.rs
+++ core/activity/src/provider/service.rs
@@ -11,46 +11,15 @@ use ya_core_model::activity;
 use ya_model::activity::State;
 use ya_persistence::executor::DbExecutor;
 use ya_persistence::models::ActivityEventType;
-use ya_service_bus::{timeout::*, typed as bus, RpcMessage};
-
-struct ServiceBinder<'a, 'b> {
-    bus_addr: &'b str,
-    db: &'a DbExecutor,
-}
-
-impl<'a, 'b> ServiceBinder<'a, 'b> {
-    fn bind<F: 'static, Msg: RpcMessage, Output: 'static>(self, f: F) -> Self
-    where
-        F: Fn(DbExecutor, String, Msg) -> Output,
-        Output: Future<Output = std::result::Result<Msg::Item, Msg::Error>>,
-        Msg::Error: std::fmt::Debug,
-    {
-        let db = self.db.clone();
-        let _ = bus::bind_with_caller(self.bus_addr, move |addr, msg| {
-            log::debug!("Received call to {}", Msg::ID);
-            let fut = f(db.clone(), addr, msg);
-            fut.map(|res| {
-                match &res {
-                    Ok(_) => log::debug!("Call to {} successful", Msg::ID),
-                    Err(e) => log::debug!("Call to {} failed: {:?}", Msg::ID, e),
-                }
-                res
-            })
-        });
-        self
-    }
-}
+use ya_service_bus::{timeout::*, typed::ServiceBinder};
 
 pub fn bind_gsb(db: &DbExecutor) {
     // public for remote requestors interactions
-    let _ = ServiceBinder {
-        bus_addr: activity::BUS_ID,
-        db,
-    }
-    .bind(create_activity_gsb)
-    .bind(destroy_activity_gsb)
-    .bind(get_activity_state_gsb)
-    .bind(get_activity_usage_gsb);
+    ServiceBinder::new(activity::BUS_ID, db, ())
+        .bind(create_activity_gsb)
+        .bind(destroy_activity_gsb)
+        .bind(get_activity_state_gsb)
+        .bind(get_activity_usage_gsb);
 
     local::bind_gsb(db);
 }
@@ -159,12 +128,9 @@ mod local {
 
     pub fn bind_gsb(db: &DbExecutor) {
         // local for ExeUnit interactions
-        let _ = ServiceBinder {
-            bus_addr: activity::local::BUS_ID,
-            db,
-        }
-        .bind(set_activity_state_gsb)
-        .bind(set_activity_usage_gsb);
+        ServiceBinder::new(activity::BUS_ID, db, ())
+            .bind(set_activity_state_gsb)
+            .bind(set_activity_usage_gsb);
     }
 
     /// Pass activity state (which may include error details).
