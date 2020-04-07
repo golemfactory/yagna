97fce5af2969d2a94f37f48662f6a5c5c4968fc3
Piotr Chromiec <tworec@golem.network>
2020-04-07 09:45:49 +0200

    [exe-unit] cmd error to have both: stdout and stderr + fixed sample commands.json

--- exe-unit/src/lib.rs
+++ exe-unit/src/lib.rs
@@ -201,7 +201,7 @@ impl<R: Runtime> ExeUnit<R> {
 
         let exec_result = runtime.send(ExecCmd(ctx.cmd.clone())).await??;
         if let CommandResult::Error = exec_result.result {
-            return Err(Error::command(&ctx.cmd, exec_result.stderr.clone()));
+            return Err(Error::CommandError(exec_result));
         }
 
         let sanity_state = addr.send(GetState {}).await?.0;
