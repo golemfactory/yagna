e80038a6ef29a00522b4e6e766cf1101bf39e3aa
Marek Franciszkiewicz <marek@golem.network>
2020-04-09 09:13:03 +0200

    [ya-client] fix type names (to PascalCase)

--- interfaces/client/src/lib.rs
+++ interfaces/client/src/lib.rs
@@ -13,15 +13,15 @@ pub use error::Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 pub trait ApiClient {
-    type market: web::WebInterface;
-    type activity: web::WebInterface;
-    type payment: web::WebInterface;
+    type Market: web::WebInterface;
+    type Activity: web::WebInterface;
+    type Payment: web::WebInterface;
 }
 
 pub struct Api<T: ApiClient> {
-    pub market: T::market,
-    pub activity: T::activity,
-    pub payment: T::payment,
+    pub market: T::Market,
+    pub activity: T::Activity,
+    pub payment: T::Payment,
 }
 
 pub type RequestorApi = Api<Requestor>;
@@ -31,15 +31,15 @@ pub struct Requestor;
 pub struct Provider;
 
 impl ApiClient for Requestor {
-    type market = market::MarketRequestorApi;
-    type activity = activity::ActivityRequestorApi;
-    type payment = payment::requestor::RequestorApi;
+    type Market = market::MarketRequestorApi;
+    type Activity = activity::ActivityRequestorApi;
+    type Payment = payment::requestor::RequestorApi;
 }
 
 impl ApiClient for Provider {
-    type market = market::MarketProviderApi;
-    type activity = activity::ActivityProviderApi;
-    type payment = payment::provider::ProviderApi;
+    type Market = market::MarketProviderApi;
+    type Activity = activity::ActivityProviderApi;
+    type Payment = payment::provider::ProviderApi;
 }
 
 #[cfg(feature = "cli")]
