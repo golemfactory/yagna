1f9c06393d968b1122686098fc2ff9b560ecc5e5
Piotr Chromiec <tworec@golem.network>
2020-04-17 19:00:54 +0200

    Interfaces moved to separate repo + ya-req reshaped (#212)
    * [exe-unit] examples: local agreement and commands
    
    * [exe-unit] local descriptor to use release builds
    
    * [ya-client] top level Provider and Requestor client + cli feature
    
    * [ya-client] disable timeout if not explicitly specified
    
    * [agents] description in cargo.toml
    
    * [ya-req] accept invoice for OUR single agreement + shutdown refactor
    
    * [ya-prov] Provider: ya-client cli feature dependency
    
    * [ya-client] fix type names (to PascalCase)
    
    * [exe-unit] fix test_fill_registry_from_local_exe_unit_descriptor test
    
    * moved ./interfaces to ya-client repo
    
    * [exe-unit] revert: examples: local agreement and commands
    
    * README: public API moved to ya-client
    
    * Requestor agent: support simultaneous computations (#214)
    
    * Support simultaneous computations
    
    * Quickly release the lock in process_invoice()
    Log formatting
    
    * Quicker shutdown process
    Inform when activity execution has been interrupted
    
    * [ya-req] code separation: mkt, act, pay + ya-client changes moved to ext repo
    
    Co-authored-by: Piotr Chromiec <tworec@golem.network>
    
    Co-authored-by: Marek Franciszkiewicz <marek@golem.network>
    Co-authored-by: Przemysław K. Rekucki <prekucki@rcl.pl>
    Co-authored-by: Marek Franciszkiewicz <mfranciszkiewicz@users.noreply.github.com>

--- agent/provider/src/startup_config.rs
+++ agent/provider/src/startup_config.rs
@@ -1,59 +1,29 @@
 use structopt::{clap, StructOpt};
-use url::Url;
 
-use ya_client::{
-    activity::ActivityProviderApi, market::MarketProviderApi,
-    payment::provider::ProviderApi as PaymentProviderApi, web::WebClient, web::WebInterface,
-    Result,
-};
+use ya_client::cli::ApiOpts;
 
 #[derive(StructOpt)]
+#[structopt(about = clap::crate_description!())]
 #[structopt(setting = clap::AppSettings::ColoredHelp)]
 #[structopt(setting = clap::AppSettings::DeriveDisplayOrder)]
 pub struct StartupConfig {
-    /// Yagna daemon application key
-    #[structopt(long = "app-key", env = "YAGNA_APPKEY", hide_env_values = true)]
-    pub app_key: String,
-    /// Market API URL
-    #[structopt(long = "market-url", env = MarketProviderApi::API_URL_ENV_VAR)]
-    market_url: Option<Url>,
-    /// Activity API URL
-    #[structopt(long = "activity-url", env = ActivityProviderApi::API_URL_ENV_VAR)]
-    activity_url: Option<Url>,
-    /// Payment API URL
-    #[structopt(long = "payment-url", env = PaymentProviderApi::API_URL_ENV_VAR)]
-    payment_url: Option<Url>,
+    #[structopt(flatten)]
+    pub api: ApiOpts,
+
     /// Descriptor file (JSON) for available ExeUnits
     #[structopt(
         long = "exe-unit-path",
         env = "EXE_UNIT_PATH",
+        hide_env_values = true,
         default_value = "/usr/lib/yagna/plugins/exeunits-descriptor.json"
     )]
     pub exe_unit_path: String,
-    /// Credit address. Can be set same as default identity
-    /// (will be removed in future release)
-    #[structopt(long = "credit-address", env = "CREDIT_ADDRESS")]
+    /// Credit address. Can be set same as default identity.
+    /// It will be removed in future release -- agreement will specify it.
+    #[structopt(
+        long = "credit-address",
+        env = "CREDIT_ADDRESS",
+        hide_env_values = true
+    )]
     pub credit_address: String,
-}
-
-impl StartupConfig {
-    pub fn market_client(&self) -> Result<MarketProviderApi> {
-        self.api_client(&self.market_url)
-    }
-
-    pub fn activity_client(&self) -> Result<ActivityProviderApi> {
-        self.api_client(&self.activity_url)
-    }
-
-    pub fn payment_client(&self) -> Result<PaymentProviderApi> {
-        self.api_client(&self.payment_url)
-    }
-
-    pub fn api_client<T: WebInterface>(&self, url: &Option<Url>) -> Result<T> {
-        let client = WebClient::with_token(&self.app_key)?;
-        match url.as_ref() {
-            Some(url) => Ok(client.interface_at(url.clone())),
-            None => Ok(client.interface()?),
-        }
-    }
 }
