e6c422e870365a4e8fd800ceed1458315a6447cc
Filip <filip@golem.network>
2020-04-23 14:27:20 +0200

    Work in progress

--- utils/constraints/src/lib.rs
+++ utils/constraints/src/lib.rs
@@ -1,30 +1,30 @@
+use serde::export::Formatter;
 use serde::Serialize;
+use std::fmt;
 //use std::ops::{Index, IndexMut};
 
 #[derive(Clone, Serialize)]
 pub enum Constraints {
-    Clause(ConstraintClause),
     Single(ConstraintExpr),
+    Clause(ConstraintClause),
 }
 
 type ConstraintKey = serde_json::Value;
 type ConstraintValue = serde_json::Value;
 
 impl Constraints {
-    pub fn try_from<T: Into<Constraints>>(
-        op: ClauseOperator,
-        v: Vec<T>,
-    ) -> Result<Constraints, serde_json::error::Error> {
-        Ok(Constraints::Clause(ConstraintClause {
+    pub fn new_clause<T: Into<Constraints>>(op: ClauseOperator, v: Vec<T>) -> Constraints {
+        Constraints::Clause(ConstraintClause {
             constraints: v.into_iter().map(|x| x.into()).collect(),
             operator: op,
-        }))
+        })
     }
-    /* TODO append */
+    pub fn new_single<T: Into<ConstraintExpr>>(el: T) -> Constraints {
+        Constraints::Single(el.into())
+    }
 }
 
-//v: &[T],
-// where for<'a> Constraints : From<&'a T> {
+//v: &[T],; where for<'a> Constraints : From<&'a T> {
 
 impl<T: Into<ConstraintExpr>> From<T> for Constraints {
     fn from(c: T) -> Self {
@@ -32,26 +32,67 @@ impl<T: Into<ConstraintExpr>> From<T> for Constraints {
     }
 }
 
+impl fmt::Display for Constraints {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Constraints::Single(expr) => expr.to_string(),
+                Constraints::Clause(clause) => clause.to_string(),
+            }
+        )
+    }
+}
+
 impl<T: Into<ConstraintKey>, U: Into<ConstraintValue>> From<(T, ConstraintOperator, U)>
     for ConstraintExpr
 {
     fn from((key, operator, value): (T, ConstraintOperator, U)) -> Self {
-        ConstraintExpr::new(key.into(), operator, value.into())
+        ConstraintExpr::with_key_op_value(key.into(), operator, value.into())
     }
 }
 
+/*impl<T: Into<ConstraintKey>> From<T> for ConstraintExpr
+{
+    fn from(key: T) -> Self {
+        ConstraintExpr::with_key(key.into())
+    }
+}*/
+
 #[derive(Copy, Clone, Serialize)]
 pub enum ClauseOperator {
-    #[serde(rename = "&")]
     And,
-    #[serde(rename = "|")]
     Or,
+}
+
+impl fmt::Display for ClauseOperator {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ClauseOperator::And => "&",
+                ClauseOperator::Or => "|",
+            }
+        )
+    }
 }
 
 #[derive(Clone, Serialize)]
 pub struct ConstraintClause {
     constraints: Vec<Constraints>,
     operator: ClauseOperator,
+}
+
+impl fmt::Display for ConstraintClause {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "({}\n", self.operator.to_string())?;
+        for el in &self.constraints {
+            write!(f, "  {}\n", el.to_string().replace("\n", "\n  "))?;
+        }
+        write!(f, ")")
+    }
 }
 
 /*
@@ -70,36 +111,67 @@ impl IndexMut<&str> for ConstraintClause {
 
 #[derive(Copy, Clone, Serialize)]
 pub enum ConstraintOperator {
-    #[serde(rename = "=")]
     Equal,
-    #[serde(rename = "<>")]
     NotEqual,
-    #[serde(rename = "<")]
     LessThan,
-    #[serde(rename = ">")]
     GreaterThan,
 }
 
 impl ConstraintOperator {}
 
+impl fmt::Display for ConstraintOperator {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ConstraintOperator::Equal => "=",
+                ConstraintOperator::NotEqual => "<>",
+                ConstraintOperator::LessThan => "<",
+                ConstraintOperator::GreaterThan => ">",
+            }
+        )
+    }
+}
+
 #[derive(Clone, Serialize)]
 pub struct ConstraintExpr {
     key: ConstraintKey,
-    operator: ConstraintOperator,
-    value: ConstraintValue,
+    operator: Option<ConstraintOperator>,
+    value: Option<ConstraintValue>,
 }
 
 impl ConstraintExpr {
-    fn new(
+    pub fn with_key(key: ConstraintKey) -> ConstraintExpr {
+        ConstraintExpr {
+            key,
+            operator: None,
+            value: None,
+        }
+    }
+    pub fn with_key_op_value(
         key: ConstraintKey,
         operator: ConstraintOperator,
         value: ConstraintValue,
     ) -> ConstraintExpr {
         ConstraintExpr {
             key,
-            operator,
-            value,
+            operator: Some(operator),
+            value: Some(value),
         }
+    }
+}
+
+impl fmt::Display for ConstraintExpr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "({}", self.key.as_str().unwrap_or(&self.key.to_string()))?;
+        if let Some(operator) = &self.operator {
+            write!(f, "{}", operator.to_string())?;
+        }
+        if let Some(value) = &self.value {
+            write!(f, "{}", value.as_str().unwrap_or(&value.to_string()))?
+        }
+        write!(f, ")")
     }
 }
 
@@ -109,23 +181,23 @@ mod tests {
 
     #[test]
     fn test_constraints() {
-        use serde_json::json;
         use ClauseOperator::*;
         use ConstraintOperator::*;
-        let c = Constraints::try_from(
+        let c = Constraints::new_clause(
             And,
             vec![
                 ("golem.inf.mem.gib", GreaterThan, 0.5).into(),
-                ("golem.inf.storage.gib", GreaterThan, json!(1.0)).into(),
-                ("golem.com.pricing.model", Equal, json!("linear")).into(),
-                Constraints::try_from(
-                    And,
-                    vec![("golem.inf.storage.gib", GreaterThan, json!(1.0))],
-                )
-                .unwrap(),
+                ("golem.inf.storage.gib", GreaterThan, 1.0).into(),
+                ("golem.com.pricing.model", Equal, "linear").into(),
+                Constraints::new_clause(
+                    Or,
+                    vec![
+                        ("golem.inf.storage.gib", GreaterThan, 1.0),
+                        ("golem.inf.storage.gib", GreaterThan, 2.0),
+                    ],
+                ),
             ],
-        )
-        .unwrap();
-        println!("{}", serde_json::to_string_pretty(&c).unwrap());
+        );
+        println!("{}", c.to_string());
     }
 }
