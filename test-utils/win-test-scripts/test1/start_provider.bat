d8780503abf4265360a6c30dda2809a15d54dba7
stranger80 <stranger80@users.noreply.github.com>
2020-04-24 22:05:27 +0200

    Adjusting win test scripts to recent updates (#196)
    * Adjusting win test scripts to recent updates
    
    * Adjustments to yagna exeunit and tests for Win
    
    * cargo fmt
    
    * Updated linux scripts
    
    * Tweaks to win test scripts and README
    
    * adjusting test agreement to match provider agent
    
    * Updated test scripts for master branch changes
    
    * Adjusting tests to latest yagna/master
    
    * Corrected input/output paths in exescript
    
    * cargo fmt
    
    * Reworked filename sanitization logic
    
    * Cleanup of unneeded files
    
    * Refactored unit test into module.
    
    * Configuring Win-specifc unit tests
    
    * Another attempt to fix file paths on Win
    
    * cargo fmt
    
    * reduce windows workarounds influence only to windows platform
    
    * fix
    
    * fix
    
    * fix
    
    * f**kn fix
    
    Co-authored-by: MBarwicki <MBarwicki@bravurasolutions.com>
    Co-authored-by: Piotr Chromiec <tworec@golem.network>

--- test-utils/win-test-scripts/test1/start_provider.bat
+++ test-utils/win-test-scripts/test1/start_provider.bat
@@ -1,3 +1,3 @@
 set RUST_LOG=info
-curl -X POST "http://localhost:5001/admin/import-key" -H "accept: application/json" -H "Content-Type: application/json-patch+json" -d "[ { \"key\": \"45910305c4f3437fb3dd64031975b817\", \"nodeId\": \"0x07c492fe0a903c582b59dab769876df686641670\" }]"
-cargo run --bin ya-provider -- --activity-url http://127.0.0.1:6001/activity-api/v1/ --app-key 45910305c4f3437fb3dd64031975b817 --market-url http://127.0.0.1:5001/market-api/v1/ --exe-unit-path ..\..\..\exe-unit
+set GSB_URL=tcp://127.0.0.1:6011
+cargo run --bin ya-provider -- --exe-unit-path local-exeunits-descriptor.json run amazing-offer --credit-address 0x213123123 --payment-url http://127.0.0.1:6001/payment-api/v1/ --activity-url http://127.0.0.1:6001/activity-api/v1/ --app-key 8fec2b8fce0f471ea7182fe76b6781f2 --market-url http://127.0.0.1:5001/market-api/v1/ --node-name Provider_Node 
