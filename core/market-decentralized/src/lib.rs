139366559dd68073a1cd8a49e35a62e8621b3cb2
Piotr Chromiec <tworec@golem.network>
2020-05-14 10:04:37 +0200

    [market] two market crates as compilation features

--- core/market-decentralized/src/lib.rs
+++ core/market-decentralized/src/lib.rs
@@ -9,6 +9,25 @@ mod matcher;
 mod negotiation;
 
 pub mod protocol;
+pub mod service {
+    use ya_persistence::executor::DbExecutor;
+    use ya_service_api_interfaces::{Provider, Service};
+
+    pub struct MarketService;
+
+    impl Service for MarketService {
+        type Cli = ();
+    }
+
+    impl MarketService {
+        pub fn rest(db: &DbExecutor) -> actix_web::Scope {
+            unimplemented!()
+        }
+        pub async fn gsb<Context: Provider<Self, DbExecutor>>(ctx: &Context) -> anyhow::Result<()> {
+            unimplemented!()
+        }
+    }
+}
 
 pub use market::Market;
 pub use ya_client_model::market::MARKET_API_PATH;
