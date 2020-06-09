73ef8cc366ee0f7fb9ce92ed1aa1169c9bead5e4
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-06-09 12:03:13 +0200

    Add CallbackMessage trait to replace RpcMessage to avoid confusion

--- core/market/decentralized/src/protocol/mod.rs
+++ core/market/decentralized/src/protocol/mod.rs
@@ -6,4 +6,4 @@ pub use self::discovery::{Discovery, Propagate, StopPropagateReason};
 pub use self::discovery::{DiscoveryError, DiscoveryInitError, DiscoveryRemoteError};
 pub use self::discovery::{OfferReceived, OfferUnsubscribed, RetrieveOffers};
 
-pub use self::callbacks::{CallbackHandler, HandlerSlot};
+pub use self::callbacks::{CallbackHandler, CallbackMessage, HandlerSlot};
