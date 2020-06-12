9ec33797572d61ef00359f95efc3a825675df256
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-06-12 14:32:48 +0200

    Cargo fmt

--- core/market/decentralized/tests/utils/mod.rs
+++ core/market/decentralized/tests/utils/mod.rs
@@ -1,10 +1,10 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
+pub mod bcast;
 mod mock_net;
 pub mod mock_node;
 pub mod mock_offer;
-pub mod bcast;
 
 pub use mock_node::MarketsNetwork;
 pub use mock_offer::{example_demand, example_offer};
