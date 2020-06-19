6fb4b946af923886c21b53530740513a2b4890a4
Daniel Bigos <daniel.bigos@golem.network>
2020-06-19 14:06:58 +0200

    chore: spawn notification job instead of waiting

--- core/identity/src/service/identity.rs
+++ core/identity/src/service/identity.rs
@@ -55,11 +55,14 @@ fn send_event(s: Ref<Subscription>, event: model::event::Event) -> impl Future<O
 
     async move {
         for endpoint in subscriptions {
-            match bus::service(&endpoint).call(event.clone()).await {
-                Err(e) => log::error!("failed to send event: {}", e),
-                Ok(Err(e)) => log::error!("failed to send event: {}", e),
-                Ok(Ok(_)) => log::debug!("sent event: {:?} to {}", event, endpoint),
-            }
+            let msg = event.clone();
+            Arbiter::spawn(async move {
+                match bus::service(&endpoint).call(msg.clone()).await {
+                    Err(e) => log::error!("failed to send event: {}", e),
+                    Ok(Err(e)) => log::error!("failed to send event: {}", e),
+                    Ok(Ok(_)) => log::debug!("sent event: {:?} to {}", msg, endpoint),
+                }
+            });
         }
     }
 }
