7f212faa6daee16686a0454344b94fcc0c49bfc4
Piotr Chromiec <tworec@golem.network>
2020-07-01 13:20:38 +0200

    [mkt] separated store from matcher (#371)
    - `SubscriptionStore` separated from `Matcher`
    - Propagation logic enclosed within Matcher (handlers)
    - Matcher is not clonable (it is not needed)
    - better error messages
    - tests adapted + macro for asserting errors

--- core/market/decentralized/src/db/dao/offer.rs
+++ core/market/decentralized/src/db/dao/offer.rs
@@ -1,12 +1,10 @@
-use chrono::Utc;
-use thiserror::Error;
+use chrono::NaiveDateTime;
 
-use ya_persistence::executor::Error as DbError;
 use ya_persistence::executor::{
     do_with_transaction, readonly_transaction, AsDao, ConnType, PoolType,
 };
 
-use crate::db::models::{Offer as ModelOffer, OfferUnsubscribed};
+use crate::db::models::{Offer, OfferUnsubscribed};
 use crate::db::schema::market_offer::dsl;
 use crate::db::schema::market_offer_unsubscribed::dsl as dsl_unsubscribed;
 use crate::db::DbResult;
@@ -23,59 +21,34 @@ impl<'c> AsDao<'c> for OfferDao<'c> {
     }
 }
 
-#[derive(Error, Debug)]
-pub enum UnsubscribeError {
-    #[error("Can't unsubscribe not existing offer")]
-    OfferNotFound,
-    #[error("Can't unsubscribe expired offer")]
-    OfferExpired,
-    #[error("Offer already unsubscribed")]
-    AlreadyUnsubscribed,
-    #[error("Can't unsubscribe offer. Database error: {0}")]
-    DatabaseError(DbError),
-}
-
 /// Internal Offer state.
 ///
 /// Since we keep only Offers subscribed locally
 /// (Offers from other nodes are removed upon unsubscribe)
 /// Unsubscribed and Expired Offers are Options.
 // TODO: cleanup external expired offers
+#[derive(Clone)]
 pub enum OfferState {
-    Active(ModelOffer),
-    Unsubscribed(Option<ModelOffer>),
-    Expired(Option<ModelOffer>),
+    Active(Offer),
+    Unsubscribed(Option<Offer>),
+    Expired(Option<Offer>),
     NotFound,
 }
 
 impl<'c> OfferDao<'c> {
-    pub async fn get_offer(
+    pub async fn select(
         &self,
         subscription_id: &SubscriptionId,
-    ) -> DbResult<Option<ModelOffer>> {
+        validation_ts: NaiveDateTime,
+    ) -> DbResult<OfferState> {
         let subscription_id = subscription_id.clone();
-        let now = Utc::now().naive_utc();
-
         readonly_transaction(self.pool, move |conn| {
-            match query_offer(conn, &subscription_id)? {
-                OfferState::Active(model_offer) => Ok(Some(model_offer)),
-                _ => Ok(None),
-            }
+            query_state(conn, &subscription_id, validation_ts)
         })
         .await
     }
 
-    pub async fn get_offer_state(&self, subscription_id: &SubscriptionId) -> DbResult<OfferState> {
-        let subscription_id = subscription_id.clone();
-        readonly_transaction(self.pool, move |conn| query_offer(conn, &subscription_id)).await
-    }
-
-    pub async fn create_offer(&self, offer: &ModelOffer) -> DbResult<()> {
-        let mut offer = offer.clone();
-        // Insertions timestamp should always reference our local time
-        // of adding it to database, so we must reset it here.
-        offer.insertion_ts = None;
-
+    pub async fn insert(&self, offer: Offer) -> DbResult<()> {
         do_with_transaction(self.pool, move |conn| {
             diesel::insert_into(dsl::market_offer)
                 .values(offer)
@@ -85,28 +58,30 @@ impl<'c> OfferDao<'c> {
         .await
     }
 
-    pub async fn mark_offer_as_unsubscribed(
+    /// In single transaction checks offer state and if `Active` inserts unsubscription marker.
+    /// Returns state as before operation. `Active` means unsubscribe has succeeded.
+    pub async fn mark_unsubscribed(
         &self,
         subscription_id: &SubscriptionId,
-    ) -> Result<(), UnsubscribeError> {
+        validation_ts: NaiveDateTime,
+    ) -> DbResult<OfferState> {
         let subscription_id = subscription_id.clone();
-        Ok(do_with_transaction(self.pool, move |conn| {
-            match query_offer(conn, &subscription_id)? {
+        do_with_transaction(self.pool, move |conn| {
+            query_state(conn, &subscription_id, validation_ts).map(|state| match state {
                 OfferState::Active(offer) => {
                     diesel::insert_into(dsl_unsubscribed::market_offer_unsubscribed)
-                        .values(offer.into_unsubscribe())
-                        .execute(conn)?;
-                    Ok(())
+                        .values(offer.clone().into_unsubscribe())
+                        .execute(conn)
+                        .map_err(From::from)
+                        .map(|_| OfferState::Active(offer))
                 }
-                OfferState::Expired(_) => Err(UnsubscribeError::OfferExpired),
-                OfferState::Unsubscribed(_) => Err(UnsubscribeError::AlreadyUnsubscribed),
-                OfferState::NotFound => Err(UnsubscribeError::OfferNotFound),
-            }
+                _ => Ok(state),
+            })
         })
-        .await?)
+        .await?
     }
 
-    pub async fn remove_offer(&self, subscription_id: &SubscriptionId) -> DbResult<bool> {
+    pub async fn delete(&self, subscription_id: &SubscriptionId) -> DbResult<bool> {
         let subscription_id = subscription_id.clone();
 
         do_with_transaction(self.pool, move |conn| {
@@ -118,8 +93,12 @@ impl<'c> OfferDao<'c> {
     }
 }
 
-fn query_offer(conn: &ConnType, subscription_id: &SubscriptionId) -> DbResult<OfferState> {
-    let offer: Option<ModelOffer> = dsl::market_offer
+fn query_state(
+    conn: &ConnType,
+    subscription_id: &SubscriptionId,
+    validation_ts: NaiveDateTime,
+) -> DbResult<OfferState> {
+    let offer: Option<Offer> = dsl::market_offer
         .filter(dsl::id.eq(&subscription_id))
         .first(conn)
         .optional()?;
@@ -130,9 +109,9 @@ fn query_offer(conn: &ConnType, subscription_id: &SubscriptionId) -> DbResult<Of
 
     Ok(match offer {
         None => OfferState::NotFound,
-        Some(offer) => match offer.expiration_ts > Utc::now().naive_utc() {
+        Some(offer) => match offer.expiration_ts > validation_ts {
             true => OfferState::Active(offer),
-            false => OfferState::Expired(Some(offer)), // TODO: cleanup external expired offers
+            false => OfferState::Expired(Some(offer)),
         },
     })
 }
@@ -143,10 +122,4 @@ fn is_unsubscribed(conn: &ConnType, subscription_id: &SubscriptionId) -> DbResul
         .first::<OfferUnsubscribed>(conn)
         .optional()?
         .is_some())
-}
-
-impl<ErrorType: Into<DbError>> From<ErrorType> for UnsubscribeError {
-    fn from(e: ErrorType) -> Self {
-        UnsubscribeError::DatabaseError(e.into())
-    }
 }
