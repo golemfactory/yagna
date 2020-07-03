35a67dd08e829b5819d50ba626c2de6a728504dd
Piotr Chromiec <tworec@golem.network>
2020-07-03 13:25:55 +0200

    [git-ver] apply @mf revie suggestion

--- utils/compile-time-utils/src/lib.rs
+++ utils/compile-time-utils/src/lib.rs
@@ -6,8 +6,7 @@ macro_rules! crate_version_commit {
         Box::leak(
             [
                 env!("CARGO_PKG_VERSION"),
-                "-",
-                ya_compile_time_utils::git_version!(),
+                ya_compile_time_utils::git_version!(prefix = "-", fallback = ""),
             ]
             .join("")
             .into_boxed_str(),
