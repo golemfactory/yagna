6f23494e5109198b80140bbcacd05ea3c5afb5ae
Marek Franciszkiewicz <marek@golem.network>
2020-07-15 17:06:21 +0200

    Various path-related fixes
    Provider:
    - create `work` and `cache` sub-directories in a working directory
    - use any existing path for hw configuration
    
    Transfer:
    - use url-decoded Url::path()
    
    Deploy package path:
    - fix deploy image's SHA-3 sum

--- utils/transfer/src/file.rs
+++ utils/transfer/src/file.rs
@@ -1,6 +1,7 @@
 use crate::error::Error;
 use crate::{
     abortable_sink, abortable_stream, TransferData, TransferProvider, TransferSink, TransferStream,
+    UrlExt,
 };
 use bytes::BytesMut;
 use futures::future::ready;
@@ -18,33 +19,20 @@ impl Default for FileTransferProvider {
     }
 }
 
-#[cfg(windows)]
-fn extract_file_url(url: &Url) -> String {
-    // On Windows, Rust implementation of Url::parse() adds a third '/' after the 'file://' indicator,
-    // thus making .path() method unusable for the purposes of file creation (because File::create() will not accept that),
-    // and therefore - Url hardly usable for carrying absolute file paths...
-    url.as_str().to_owned().replace("file:///", "")
-}
-
-#[cfg(not(windows))]
-fn extract_file_url(url: &Url) -> String {
-    url.path().to_owned()
-}
-
 impl TransferProvider<TransferData, Error> for FileTransferProvider {
     fn schemes(&self) -> Vec<&'static str> {
         vec!["file"]
     }
 
     fn source(&self, url: &Url) -> TransferStream<TransferData, Error> {
-        let url = extract_file_url(url);
-
         let (stream, tx, abort_reg) = TransferStream::<TransferData, Error>::create(1);
         let txc = tx.clone();
+        let url = url.clone();
 
         tokio::task::spawn_local(async move {
             let fut = async move {
-                let file = File::open(url).await?;
+                let path = url.path_decoded();
+                let file = File::open(&path).await?;
                 FramedRead::new(file, BytesCodec::new())
                     .map_ok(BytesMut::freeze)
                     .map_err(Error::from)
@@ -64,17 +52,15 @@ impl TransferProvider<TransferData, Error> for FileTransferProvider {
     }
 
     fn destination(&self, url: &Url) -> TransferSink<TransferData, Error> {
-        let url = extract_file_url(url);
-
-        log::info!("destination url: {}", url);
-
         let (sink, mut rx, res_tx) = TransferSink::<TransferData, Error>::create(1);
+        let url = url.clone();
+        let url_f = url.clone();
 
         //TODO: Return Result from function if file creation fails.
         tokio::task::spawn_local(async move {
-            let local_url = url.clone();
+            let path = url_f.path_decoded();
             let fut = async move {
-                let mut file = File::create(url.clone()).await?;
+                let mut file = File::create(&path).await?;
                 while let Some(result) = rx.next().await {
                     file.write_all(&result?.into_bytes()).await?;
                 }
@@ -83,7 +69,7 @@ impl TransferProvider<TransferData, Error> for FileTransferProvider {
                 Result::<(), Error>::Ok(())
             }
             .map_err(|error| {
-                log::error!("Error opening destination file [{}]: {}", local_url, error);
+                log::error!("Error opening destination file [{}]: {}", url, error);
                 Error::from(error)
             });
 
