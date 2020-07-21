91019baf916b811b3e898ca067c55f995d34c391
Piotr Chromiec <tworec@golem.network>
2020-07-21 14:37:43 +0200

    [ya-prov] setup script: apply review comments
    - allow specyfing cargo arguments (default: decentralized market feature)
    - remove pid file
    - cleanup

--- agent/provider/build-run-ya-provider.sh
+++ agent/provider/build-run-ya-provider.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 INSTALL_DIR=~/.yagna
+CARGO_ARGS="--features market-decentralized --no-default-features"
 
 usage() {
   cat 1>&2 <<EOF
 Runs Yagna Service and ya-provider from sources.
 
 USAGE:
-    $(basename $0) <install_dir>
+    $(basename $0) -d <install_dir> <yagna-cargo-build-args>
 
-Files are being placed in '$INSTALL_DIR' by default.
-You can pass another location as a first argument.
-
+OPTIONS:
+    -d <install_dir>
+        installation location ('$INSTALL_DIR' by default).
+    <yagna-cargo-build-args>
+        additional arguments passed to cargo build for yagna
+        ('$CARGO_ARGS' by default)
 
 Steps:
     1. Installs or updates rust
-    2. Clones and builds ya-runtime-wasi
-    3. Clones and builds yagna
+    2. Clones (or updates) and builds ya-runtime-wasi
+    3. Clones (or updates) and builds yagna
     4. Runs Yagna Service
     5. Configures and runs ya-provider.
 EOF
@@ -26,7 +30,7 @@ EOF
 ## helpers, adapted from from sh.rustup.rs ##
 
 say() {
-    printf 'yagna-up[%s]: %s\n' "$(date +'%Y-%m-%dT%H:%M:%S%z')" "$@"
+    printf 'yagna-up [%s]: %s\n' "$(date +'%Y-%m-%d %H:%M:%S')" "$@"
 }
 
 err() {
@@ -64,59 +68,65 @@ main() {
     need_cmd curl
     need_cmd git
 
-    if [[ "$1" == "-h" ]]; then
-        usage
-        exit 0
-    fi
-
-    install_dir=${1:-$INSTALL_DIR}
+    local install_dir="$INSTALL_DIR"
+    local cargo_args="$CARGO_ARGS"
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+            -h|--help) usage; exit 0 ;;
+            -d) install_dir="$2"; shift ;;
+            *) cargo_args="$*"; break ;;
+        esac
+        shift
+    done
 
     if ! check_cmd rustup; then
-        say "install rust"
+        say "Install rust"
         curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 	      export PATH="$HOME/.cargo/bin:$PATH"
     fi
     rustup update
 
     if [[ ! -d "$install_dir" ]]; then
-        say "creating install dir $install_dir"
-        mkdir "$install_dir"
+        say "Creating install dir $install_dir"
+        ensure mkdir "$install_dir"
+    else
+        say "Using install dir $install_dir"
     fi
 
     cd "$install_dir"
 
     if [[ ! -d ya-runtime-wasi ]]; then
-    	  say "clone WASI runtime"
+    	  say "Clone WASI runtime"
         ensure git clone https://github.com/golemfactory/ya-runtime-wasi.git
     fi
 
     cd ya-runtime-wasi
-    git pull
-    say "build WASI runtime"
+    say "Update WASI runtime sources"
+    ensure git pull
+    say "Build WASI runtime"
     ensure cargo build
     cd ..
 
     if [[ ! -d yagna ]]; then
-        say "clone main yagna repo"
+        say "Clone main yagna repo"
         ensure git clone https://github.com/golemfactory/yagna.git
     fi
 
     cd yagna
-    git pull
+    say "Update Yagna sources"
+    ensure git pull
     #git checkout exe-unit/init-logs
 
-    say "build ExeUnit supervisor, Provider and Yagna (with decentralized marketplace)"
-    ensure cargo build -p ya-exe-unit -p ya-provider -p yagna \
-        --features market-decentralized --no-default-features
+    say "Build ExeUnit supervisor, Provider and Yagna (with $cargo_args)"
+    ensure cargo build -p ya-exe-unit -p ya-provider -p yagna $cargo_args
 
     local prov_dir="ya-prov"
-
     mkdir -p "$prov_dir"
     if [[ ! -f "$prov_dir/.env" ]]; then
-        say "configure ya-provider"
+        say "Configure ya-provider"
 
         cp .env-template "$prov_dir/.env"
-	      sed \
+	      ensure sed \
             -e "s|#GSB_URL=tcp://127.0.0.1:7464|GSB_URL=tcp://127.0.0.1:17474|" \
             -e "s|#YAGNA_API_URL=http://127.0.0.1:7465|YAGNA_API_URL=http://127.0.0.1:17475|" \
 	          -e "s|__YOUR_NODE_NAME_GOES_HERE__|${USER}@${HOSTNAME}-ya-mkt-dece|" \
@@ -127,37 +137,38 @@ main() {
 
     local pid_file="yagna.pid"
     if [[ -f "$pid_file" ]]; then
-	      say "getting pid"
+	      say "Getting pid"
         local pid=$(cat "$pid_file")
-	      say "killing Yagna service ($pid)..."
+	      say "Killing Yagna service ($pid)..."
         ignore kill "$pid"
+        ignore rm -f "$pid_file"
         sleep 2s # wait a bit for service to finish cleanly
         say "Yagna service killed."
     fi
 
-    say "starting Yagna Service... (stdout & err in yagna.log)"
+    say "Starting Yagna Service... (stdout & err in yagna.log)"
     ../target/debug/yagna service run >> yagna.log 2>&1 &
-    echo "$!" > "$pid_file"
-    local pid=$(cat "$pid_file")
+    local pid="$!"
+    echo "$pid" > "$pid_file"
     sleep 2s # wait a bit for service to start fully
     say "Yagna Service started ($pid)."
 
     if grep "__GENERATED_APP_KEY__" .env > /dev/null; then
-        say "generate app key and place it within .env"
+        say "Generate app key and place it within .env"
 	      ignore ../target/debug/yagna app-key drop 'provider-agent'
 	      APP_KEY=$(ensure ../target/debug/yagna app-key create 'provider-agent')
 	      if [[ -z "$APP_KEY" ]]; then
-            err "app key not generated"
+            err "App key not generated"
 	      fi
         sed -e "s/__GENERATED_APP_KEY__/$APP_KEY/" -i.bckp .env
     fi
 
-    say "register provider's payment account"
+    say "Register provider's payment account"
     ensure ../target/debug/yagna payment init gnt -p
 
     sleep 10s # wait for other nodes (optional)
 
-    say "start the Provider Agent"
+    say "Start the Provider Agent"
     ignore ../target/debug/ya-provider --data-dir . run
 }
 
