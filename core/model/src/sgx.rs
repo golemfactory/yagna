0be68d9292bdc62a8eea8a97eb0d91e31c23f984
Rafał Wojdyła <omeg@invisiblethingslab.com>
2020-08-24 09:00:05 +0200

    Change sgx gsb endpoint to be local

--- core/model/src/sgx.rs
+++ core/model/src/sgx.rs
@@ -1,24 +1,4 @@
-pub use sgx_types::ias::AttestationResponse;
 use serde::{Deserialize, Serialize};
-use ya_service_bus::RpcMessage;
-
-/// Public SGX bus address.
-pub const BUS_ID: &str = "/public/sgx";
-
-#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct VerifyAttestationEvidence {
-    pub production: bool,
-    pub ias_api_key: String,
-    pub ias_nonce: Option<String>,
-    pub enclave_quote: Vec<u8>,
-}
-
-impl RpcMessage for VerifyAttestationEvidence {
-    const ID: &'static str = "VerifyAttestationEvidence";
-    type Item = AttestationResponse;
-    type Error = Error;
-}
 
 /// Error message for SGX service bus API.
 #[derive(thiserror::Error, Clone, Debug, Serialize, Deserialize)]
@@ -26,4 +6,28 @@ impl RpcMessage for VerifyAttestationEvidence {
 pub enum Error {
     #[error("Attestation error: {0}")]
     Attestation(String),
+}
+
+pub mod local {
+    use super::*;
+    pub use sgx_types::ias::AttestationResponse;
+    use ya_service_bus::RpcMessage;
+
+    /// Local SGX bus address.
+    pub const BUS_ID: &str = "/local/sgx";
+
+    #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct VerifyAttestationEvidence {
+        pub production: bool,
+        pub ias_api_key: String,
+        pub ias_nonce: Option<String>,
+        pub enclave_quote: Vec<u8>,
+    }
+
+    impl RpcMessage for VerifyAttestationEvidence {
+        const ID: &'static str = "VerifyAttestationEvidence";
+        type Item = AttestationResponse;
+        type Error = Error;
+    }
 }
--- core/sgx/src/service.rs
+++ core/sgx/src/service.rs
@@ -1,19 +1,26 @@
-use anyhow::Result;
-use graphene::ias::IasClient;
-use ya_core_model::sgx::{AttestationResponse, Error, VerifyAttestationEvidence, BUS_ID};
-use ya_service_bus::typed as bus;
-
 pub fn bind_gsb() {
-    let _ = bus::bind(&BUS_ID, verify_attestation_evidence);
+    local::bind_gsb();
 }
 
-async fn verify_attestation_evidence(
-    msg: VerifyAttestationEvidence,
-) -> Result<AttestationResponse, Error> {
-    let ias = IasClient::new(msg.production);
-    let response = ias
-        .verify_attestation_evidence(&msg.enclave_quote, &msg.ias_api_key, msg.ias_nonce)
-        .await
-        .map_err(|e| Error::Attestation(format!("{}", e)))?;
-    Ok(response)
+mod local {
+    use anyhow::Result;
+    use graphene::ias::IasClient;
+    use ya_core_model::sgx::Error;
+    use ya_core_model::sgx::local::{AttestationResponse, VerifyAttestationEvidence, BUS_ID};
+    use ya_service_bus::typed as bus;
+
+    pub fn bind_gsb() {
+        let _ = bus::bind(&BUS_ID, verify_attestation_evidence);
+    }
+
+    async fn verify_attestation_evidence(
+        msg: VerifyAttestationEvidence,
+    ) -> Result<AttestationResponse, Error> {
+        let ias = IasClient::new(msg.production);
+        let response = ias
+            .verify_attestation_evidence(&msg.enclave_quote, &msg.ias_api_key, msg.ias_nonce)
+            .await
+            .map_err(|e| Error::Attestation(format!("{}", e)))?;
+        Ok(response)
+    }
 }
