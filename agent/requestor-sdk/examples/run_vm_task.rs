fc9222e904407d263fae637a723eba348973de65
mfranciszkiewicz <marek.franciszkiewicz@imapp.pl>
2020-08-31 11:32:10 +0200

    Fix error: reached the type-length limit while instantiating `std::pin::Pin::<&mut std::future...::Future>::poll::{{closure}}#0]>`

--- agent/requestor-sdk/examples/run_vm_task.rs
+++ agent/requestor-sdk/examples/run_vm_task.rs
@@ -1,5 +1,5 @@
-use anyhow::{anyhow, Result};
-use futures::{future::FutureExt, pin_mut, select};
+use anyhow::Result;
+use futures::future::{select, FutureExt};
 use std::collections::HashMap;
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -56,14 +56,17 @@ async fn main() -> Result<()> {
                 println!("{} => {}", prov_id, output);
             }
         })
-        .run()
-        .fuse();
-    let ctrl_c = actix_rt::signal::ctrl_c().fuse();
+        .run();
 
-    pin_mut!(requestor, ctrl_c);
+    let ctrl_c = actix_rt::signal::ctrl_c().then(|r| async move {
+        match r {
+            Ok(_) => Ok(log::warn!("interrupted: ctrl-c detected!")),
+            Err(e) => Err(anyhow::Error::from(e)),
+        }
+    });
 
-    select! {
-        comp_res = requestor => comp_res,
-        _ = ctrl_c => Err(anyhow!("interrupted: ctrl-c detected!")),
-    }
+    select(requestor.boxed_local(), ctrl_c.boxed_local())
+        .await
+        .into_inner()
+        .0
 }
