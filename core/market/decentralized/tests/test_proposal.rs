e855b318ae5c3c6f3239e7022640eef1cac69165
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2020-09-01 12:02:49 +0200

    [Test] Get not existing Proposal

--- core/market/decentralized/tests/test_proposal.rs
+++ core/market/decentralized/tests/test_proposal.rs
@@ -1,5 +1,5 @@
 use ya_market_decentralized::testing::proposal_util::exchange_draft_proposals;
-use ya_market_decentralized::testing::{MarketsNetwork, OwnerType};
+use ya_market_decentralized::testing::{GetProposalError, MarketsNetwork, OwnerType};
 
 use ya_client::model::market::proposal::State;
 
@@ -41,5 +41,36 @@ async fn test_get_proposal_rest() -> Result<(), anyhow::Error> {
     assert_eq!(proposal.proposal_id()?, &proposal_id.to_string());
     assert_eq!(proposal.issuer_id()?, &prov_id.identity.to_string());
     assert!(proposal.prev_proposal_id().is_ok());
+    Ok(())
+}
+
+/// Try to query not existing Proposal.
+#[cfg_attr(not(feature = "market-test-suite"), ignore)]
+#[actix_rt::test]
+async fn test_get_proposal_not_found() -> Result<(), anyhow::Error> {
+    let network = MarketsNetwork::new("test_get_proposal_rest")
+        .await
+        .add_market_instance("Requestor1")
+        .await?
+        .add_market_instance("Provider1")
+        .await?;
+
+    let req_market = network.get_market("Requestor1");
+    let req_id = network.get_default_id("Requestor1");
+
+    // Create some Proposals, that will be unused.
+    exchange_draft_proposals(&network, "Requestor1", "Provider1").await?;
+
+    // Invalid id
+    let proposal_id = "P-0000000000000000000000000000000000000000000000000000000000000003"
+        .parse()
+        .unwrap();
+    let result = req_market.get_proposal(&proposal_id, &req_id).await;
+
+    assert!(result.is_err());
+    assert_eq!(
+        result.unwrap_err().to_string(),
+        GetProposalError::NotFound(proposal_id).to_string(),
+    );
     Ok(())
 }
