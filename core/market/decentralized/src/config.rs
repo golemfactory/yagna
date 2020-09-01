257e8d3ffbe5ed4e4664bddb2599ee1fdb531770
Piotr Chromiec <tworec@golem.network>
2020-09-01 23:43:29 +0200

    default config simplified

--- core/market/decentralized/src/config.rs
+++ core/market/decentralized/src/config.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 /// TODO: Decide where should this config be loaded from.
 ///  We could deserialize it from .json file or use structopt and
 ///  configure market through env variables.
+#[derive(Default)]
 pub struct Config {
     pub discovery: DiscoveryConfig,
     pub subscription: SubscriptionConfig,
@@ -11,21 +12,12 @@ pub struct Config {
 pub struct DiscoveryConfig {
     pub num_broadcasted_offers: u32,
     pub num_broadcasted_unsubscribes: u32,
-    pub mean_cyclic_broadcast_interval: std::time::Duration,
-    pub mean_cyclic_unsubscribes_interval: std::time::Duration,
+    pub mean_cyclic_broadcast_interval: Duration,
+    pub mean_cyclic_unsubscribes_interval: Duration,
 }
 
 pub struct SubscriptionConfig {
     pub default_ttl: chrono::Duration,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            discovery: DiscoveryConfig::default(),
-            subscription: SubscriptionConfig::default(),
-        }
-    }
 }
 
 impl Default for DiscoveryConfig {
