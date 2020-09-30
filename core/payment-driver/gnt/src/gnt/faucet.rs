9d53eb013216192f0e48f8addb80f21f112c8595
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-09-30 12:46:31 +0200

    Resolve IP manually to fix windows 10 faucet issues

--- core/payment-driver/gnt/src/gnt/faucet.rs
+++ core/payment-driver/gnt/src/gnt/faucet.rs
@@ -1,14 +1,36 @@
 use crate::utils;
 use crate::{GNTDriverError, GNTDriverResult};
-use actix_rt::Arbiter;
 use ethereum_types::Address;
 use std::{env, time};
+use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
+use trust_dns_resolver::TokioAsyncResolver;
 
 const MAX_ETH_FAUCET_REQUESTS: u32 = 6;
 const ETH_FAUCET_SLEEP: time::Duration = time::Duration::from_secs(2);
 const INIT_ETH_SLEEP: time::Duration = time::Duration::from_secs(15);
 const ETH_FAUCET_ADDRESS_ENV_VAR: &str = "ETH_FAUCET_ADDRESS";
 const DEFAULT_ETH_FAUCET_ADDRESS: &str = "http://faucet.testnet.golem.network:4000/donate";
+
+// Hack required on windows to bypass failing trust_dns resolution on windows 10
+// Remove when https://github.com/actix/actix-web/issues/1047 is resolved
+async fn resolve_ip_manually(request_url: String) -> String {
+    let uri: awc::http::Uri = request_url.parse().unwrap();
+    let request_host = uri
+        .host()
+        .expect("Env variable 'ETH_FAUCET_ADDRESS' is not a valid URI.");
+
+    let resolver = TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default())
+        .await
+        .expect("failed to connect resolver");
+
+    let response = resolver.lookup_ip(request_host).await.unwrap();
+    let address = response
+        .iter()
+        .next()
+        .expect("Env variable 'ETH_FAUCET_ADDRESS' does not resolve in DNS.");
+
+    request_url.replace(request_host, &address.to_string())
+}
 
 pub struct EthFaucetConfig {
     faucet_address: awc::http::Uri,
@@ -27,17 +49,9 @@ impl EthFaucetConfig {
 
     pub async fn request_eth(&self, address: Address) -> GNTDriverResult<()> {
         log::debug!("request eth");
-        let (resolver, bg) = trust_dns_resolver::AsyncResolver::from_system_conf().unwrap();
-        Arbiter::spawn(bg);
-        let tcp_connector = actix_connect::new_connector(resolver);
-        let client = awc::Client::build()
-            .connector(
-                actix_http::client::Connector::new()
-                    .connector(tcp_connector)
-                    .finish(),
-            )
-            .finish();
+        let client = awc::Client::new();
         let request_url = format!("{}/{}", &self.faucet_address, utils::addr_to_str(address));
+        let request_url = resolve_ip_manually(request_url).await;
 
         async fn try_request_eth(client: &awc::Client, url: &str) -> GNTDriverResult<()> {
             let body = client
