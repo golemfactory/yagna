99daf813993bf6f22a24c5a29b3e0ba9a7b27c7e
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2020-11-09 10:46:11 +0100

    Move database and dao to payment driver base crate

---
+++ core/payment-driver/base/src/dao/payment.rs
@@ -0,0 +1,73 @@
+use diesel::{self, ExpressionMethods, QueryDsl, RunQueryDsl};
+
+use crate::db::models::{PaymentEntity, PAYMENT_STATUS_OK, PAYMENT_STATUS_NOT_YET};
+use crate::db::schema::payment::dsl;
+
+use ya_persistence::executor::{do_with_transaction, readonly_transaction, AsDao, PoolType};
+
+use crate::dao::DbResult;
+
+#[allow(unused)]
+pub struct PaymentDao<'c> {
+    pool: &'c PoolType,
+}
+
+impl<'c> AsDao<'c> for PaymentDao<'c> {
+    fn as_dao(pool: &'c PoolType) -> Self {
+        Self { pool }
+    }
+}
+
+impl<'c> PaymentDao<'c> {
+    pub async fn get_pending_payments(&self, address: String) -> DbResult<Vec<PaymentEntity>> {
+        readonly_transaction(self.pool, move |conn| {
+            let payments: Vec<PaymentEntity> = dsl::payment
+                .filter(dsl::sender.eq(address))
+                .filter(dsl::status.eq(PAYMENT_STATUS_NOT_YET))
+                .order(dsl::payment_due_date.asc())
+                .load(conn)?;
+            Ok(payments)
+        })
+        .await
+    }
+
+    pub async fn insert(&self, payment: PaymentEntity) -> DbResult<()> {
+        do_with_transaction(self.pool, move |conn| {
+            diesel::insert_into(dsl::payment)
+                .values(payment)
+                .execute(conn)?;
+            Ok(())
+        })
+        .await
+    }
+
+    pub async fn update_status(&self, order_id: String, status: i32) -> DbResult<()> {
+        do_with_transaction(self.pool, move |conn| {
+            diesel::update(dsl::payment.find(order_id))
+                .set(dsl::status.eq(status))
+                .execute(conn)?;
+            Ok(())
+        })
+        .await
+    }
+
+    pub async fn update_tx_id(&self, order_id: String, tx_id: String) -> DbResult<()> {
+        do_with_transaction(self.pool, move |conn| {
+            diesel::update(dsl::payment.find(order_id))
+                .set((dsl::tx_id.eq(tx_id), dsl::status.eq(PAYMENT_STATUS_OK)))
+                .execute(conn)?;
+            Ok(())
+        })
+        .await
+    }
+
+    pub async fn get_by_tx_id(&self, tx_id: String) -> DbResult<Vec<PaymentEntity>> {
+        readonly_transaction(self.pool, move |conn| {
+            let payments: Vec<PaymentEntity> = dsl::payment
+                .filter(dsl::tx_id.eq(tx_id))
+                .load(conn)?;
+            Ok(payments)
+        })
+        .await
+    }
+}
---
+++ core/payment-driver/base/src/db/schema.rs
@@ -0,0 +1,60 @@
+table! {
+    payment (order_id) {
+        order_id -> Text,
+        amount -> Text,
+        gas -> Text,
+        sender -> Text,
+        recipient -> Text,
+        payment_due_date -> Timestamp,
+        status -> Integer,
+        tx_id -> Nullable<Text>,
+    }
+}
+
+table! {
+    payment_status (status_id) {
+        status_id -> Integer,
+        status -> Text,
+    }
+}
+
+table! {
+    transaction (tx_id) {
+        tx_id -> Text,
+        sender -> Text,
+        nonce -> Text,
+        timestamp -> Timestamp,
+        status -> Integer,
+        tx_type -> Integer,
+        encoded -> Text,
+        signature -> Text,
+        tx_hash -> Nullable<Text>,
+    }
+}
+
+table! {
+    transaction_status (status_id) {
+        status_id -> Integer,
+        status -> Text,
+    }
+}
+
+table! {
+    transaction_type (type_id) {
+        type_id -> Integer,
+        tx_type -> Text,
+    }
+}
+
+joinable!(payment -> payment_status (status));
+joinable!(payment -> transaction (tx_id));
+joinable!(transaction -> transaction_status (status));
+joinable!(transaction -> transaction_type (tx_type));
+
+allow_tables_to_appear_in_same_query!(
+    payment,
+    payment_status,
+    transaction,
+    transaction_status,
+    transaction_type,
+);
