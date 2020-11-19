11a1ad610c7db95f80b9428c4fe7b0a7af122b70
Piotr Chromiec <tworec@golem.network>
2020-11-19 18:25:08 +0100

    latest tag (even lightweight) as default ver, cargo ver as fallback + build no

--- utils/compile-time-utils/src/lib.rs
+++ utils/compile-time-utils/src/lib.rs
@@ -5,8 +5,13 @@ macro_rules! crate_version_commit {
     () => {
         Box::leak(
             [
-                env!("CARGO_PKG_VERSION"),
-                ya_compile_time_utils::git_version!(prefix = "-", fallback = ""),
+                ya_compile_time_utils::git_version!(
+                    args = ["--tag"],
+                    fallback = env!("CARGO_PKG_VERSION")
+                ),
+                &option_env!("GITHUB_RUN_NUMBER")
+                    .map(|run_no| format!("-b{}", run_no))
+                    .unwrap_or("".to_string()),
             ]
             .join("")
             .into_boxed_str(),
