4bb3d30118a8e060d781a4a935074871ddd5a7b0
Piotr Chromiec <tworec@golem.network>
2020-11-19 22:13:07 +0100

    add latest commit, build date and build number to the version + unification

--- utils/compile-time-utils/src/lib.rs
+++ utils/compile-time-utils/src/lib.rs
@@ -1,20 +1,66 @@
-pub use git_version::git_version;
+use git_version::git_version;
+
+/// Returns latest tag or version from Cargo.toml`.
+pub fn git_tag() -> &'static str {
+    git_version!(
+        args = ["--tag", "--abbrev=0"],
+        fallback = env!("CARGO_PKG_VERSION")
+    )
+}
+
+/// Returns latest commit short hash.
+pub fn git_rev() -> &'static str {
+    env!("VERGEN_SHA_SHORT")
+}
+
+/// Returns current date in YYYY-MM-DD format.
+pub fn build_date() -> &'static str {
+    env!("VERGEN_BUILD_DATE")
+}
+
+/// Returns Github Actions build number if available or None.
+pub fn build_number() -> Option<&'static str> {
+    option_env!("GITHUB_RUN_NUMBER")
+}
+
+/// convert tag to a semantic version
+pub fn semver() -> &'static str {
+    let mut version = git_tag();
+    for prefix in ["pre-rel-", "v"].iter() {
+        if version.starts_with(prefix) {
+            version = &version[prefix.len()..];
+        }
+    }
+    version
+}
 
 #[macro_export]
-macro_rules! crate_version_commit {
+macro_rules! version_describe {
     () => {
         Box::leak(
             [
-                ya_compile_time_utils::git_version!(
-                    args = ["--tag"],
-                    fallback = env!("CARGO_PKG_VERSION")
-                ),
-                &option_env!("GITHUB_RUN_NUMBER")
-                    .map(|run_no| format!("-b{}", run_no))
+                $crate::semver(),
+                " (",
+                $crate::git_rev(),
+                " ",
+                &$crate::build_date(),
+                &$crate::build_number()
+                    .map(|n| format!(" build #{}", n))
                     .unwrap_or("".to_string()),
+                ")",
             ]
             .join("")
             .into_boxed_str(),
         ) as &str
     };
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        println!("{}", semver())
+    }
 }
