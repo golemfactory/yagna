f230768a62982763399faac495abd3b3e04392cf
nieznany.sprawiciel <witek@golem.network>
2020-11-30 18:22:53 +0100

    Decrease timeouts

--- agent/provider/src/market/config.rs
+++ agent/provider/src/market/config.rs
@@ -3,9 +3,9 @@ use structopt::StructOpt;
 /// Configuration for ProviderMarket actor.
 #[derive(StructOpt, Clone, Debug)]
 pub struct MarketConfig {
-    #[structopt(long, env, default_value = "20.0")]
+    #[structopt(long, env, default_value = "2.0")]
     pub agreement_events_interval: f32,
-    #[structopt(long, env, default_value = "20.0")]
+    #[structopt(long, env, default_value = "2.0")]
     pub negotiation_events_interval: f32,
     #[structopt(long, env, default_value = "10.0")]
     pub agreement_approve_timeout: f32,
