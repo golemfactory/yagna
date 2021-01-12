d6356d53c1ba23a8f4065d950e236b99f31e0b1a
Adam Mizerski <amizerski@golem.network>
2021-01-12 12:49:11 +0100

    bump flexi_logger version

--- agent/provider/src/provider_agent.rs
+++ agent/provider/src/provider_agent.rs
@@ -25,7 +25,7 @@ use ya_agreement_utils::agreement::TypedArrayPointer;
 use ya_agreement_utils::*;
 use ya_client::cli::ProviderApi;
 use ya_client_model::payment::Account;
-use ya_file_logging::{start_logger, ReconfigurationHandle};
+use ya_file_logging::{start_logger, LoggerHandle};
 use ya_utils_actix::actix_handler::send_message;
 use ya_utils_path::SwapSave;
 
@@ -37,7 +37,7 @@ pub struct ProviderAgent {
     presets: PresetManager,
     hardware: hardware::Manager,
     accounts: Vec<Account>,
-    log_handler: ReconfigurationHandle,
+    log_handler: LoggerHandle,
 }
 
 struct GlobalsManager {
