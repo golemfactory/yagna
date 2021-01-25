2d318372e31eba4b536e99c67fd67f4e680040ad
Piotr Chromiec <tworec@golem.network>
2021-01-25 11:25:32 +0100

    [golemsp] fix default duration price

--- golem_cli/src/setup.rs
+++ golem_cli/src/setup.rs
@@ -157,7 +157,7 @@ pub async fn setup(run_config: &mut RunConfig, force: bool) -> Result<i32> {
 
         let usage = UsageDef {
             cpu: glm_per_h / 3600.0,
-            duration: glm_per_h / 3600.0 / default_glm_per_h,
+            duration: glm_per_h / 3600.0 / 5,
             initial: 0.0,
         };
 
