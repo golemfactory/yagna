1ab6fb93d569f4e16cd31f35c94e22a0dc918a3e
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2021-03-30 15:58:40 +0200

    Removed ethkey and used the best ethsign version to match other dependencies

--- core/payment/examples/payment_api.rs
+++ core/payment/examples/payment_api.rs
@@ -1,9 +1,12 @@
 use actix_web::{middleware, App, HttpServer, Scope};
 use chrono::Utc;
-use ethkey::{EthAccount, Password};
+use ethsign::{SecretKey, KeyFile, Protected};
+use ethsign::keyfile::Bytes;
 use futures::Future;
+use rand::Rng;
 use serde_json;
 use std::convert::TryInto;
+use std::io::Write;
 use std::pin::Pin;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -89,9 +92,9 @@ pub async fn start_dummy_driver() -> anyhow::Result<()> {
 
 pub async fn start_erc20_driver(
     db: &DbExecutor,
-    requestor_account: Box<EthAccount>,
+    requestor_account: Box<SecretKey>,
 ) -> anyhow::Result<()> {
-    let requestor = NodeId::from(requestor_account.address().as_ref());
+    let requestor = NodeId::from(requestor_account.public().address().as_ref());
     fake_list_identities(vec![requestor]);
     fake_subscribe_to_events();
 
@@ -104,9 +107,9 @@ pub async fn start_erc20_driver(
 
 pub async fn start_zksync_driver(
     db: &DbExecutor,
-    requestor_account: Box<EthAccount>,
+    requestor_account: Box<SecretKey>,
 ) -> anyhow::Result<()> {
-    let requestor = NodeId::from(requestor_account.address().as_ref());
+    let requestor = NodeId::from(requestor_account.public().address().as_ref());
     fake_list_identities(vec![requestor]);
     fake_subscribe_to_events();
 
@@ -140,9 +143,9 @@ fn fake_subscribe_to_events() {
 }
 
 fn get_sign_tx(
-    account: Box<EthAccount>,
+    account: Box<SecretKey>,
 ) -> impl Fn(Vec<u8>) -> Pin<Box<dyn Future<Output = Vec<u8>>>> {
-    let account: Arc<EthAccount> = account.into();
+    let account: Arc<SecretKey> = account.into();
     move |msg| {
         let account = account.clone();
         let fut = async move {
@@ -167,6 +170,34 @@ fn fake_sign_tx(sign_tx: Box<dyn Fn(Vec<u8>) -> Pin<Box<dyn Future<Output = Vec<
     });
 }
 
+const KEY_ITERATIONS: u32 = 2;
+const KEYSTORE_VERSION: u64 = 3;
+
+fn load_or_generate(path: &str, password: Protected) -> SecretKey {
+    log::debug!("load_or_generate({}, {:?})", &path, &password);
+    if let Ok(file) = std::fs::File::open(path) {
+        log::debug!("opened file");
+
+        // Broken keyfile should panic
+        let key: KeyFile = serde_json::from_reader(file).unwrap();
+        log::debug!("parsed file");
+        // Invalid password should panic
+        return key.to_secret_key(&password).unwrap();
+    }
+    // File does not exist, create new key
+    let random_bytes: [u8; 32] = rand::thread_rng().gen();
+    let secret = SecretKey::from_raw(random_bytes.as_ref()).unwrap();
+    let key_file = KeyFile {
+        id: format!("{}", uuid::Uuid::new_v4()),
+        version: KEYSTORE_VERSION,
+        crypto: secret.to_crypto(&password, KEY_ITERATIONS).unwrap(),
+        address: Some(Bytes(secret.public().address().to_vec())),
+    };
+    let mut file = std::fs::File::create(path).unwrap();
+    file.write_all(serde_json::to_string_pretty(&key_file).unwrap().as_ref()).unwrap();
+    secret
+}
+
 #[actix_rt::main]
 async fn main() -> anyhow::Result<()> {
     if std::env::var("RUST_LOG").is_err() {
@@ -180,17 +211,17 @@ async fn main() -> anyhow::Result<()> {
 
     let args: Args = Args::from_args();
 
-    let provider_pass: Password = args.provider_pass.clone().into();
-    let provider_account = EthAccount::load_or_generate(&args.provider_key_path, provider_pass)?;
-    let provider_id = provider_account.address().to_string();
+    let provider_pass: Protected = args.provider_pass.clone().into();
+    let provider_account = Box::new(load_or_generate(&args.provider_key_path, provider_pass));
+    let provider_id = format!("0x{}", hex::encode(provider_account.public().address()));
     let provider_addr = args
         .provider_addr
         .unwrap_or(provider_id.clone())
         .to_lowercase();
 
-    let requestor_pass: Password = args.requestor_pass.clone().into();
-    let requestor_account = EthAccount::load_or_generate(&args.requestor_key_path, requestor_pass)?;
-    let requestor_id = requestor_account.address().to_string();
+    let requestor_pass: Protected = args.requestor_pass.clone().into();
+    let requestor_account = Box::new(load_or_generate(&args.requestor_key_path, requestor_pass));
+    let requestor_id = format!("0x{}", hex::encode(requestor_account.public().address()));
     let requestor_addr = args
         .requestor_addr
         .unwrap_or(requestor_id.clone())
