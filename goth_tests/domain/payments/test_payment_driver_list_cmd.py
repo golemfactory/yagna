8a738a98f7f6b83f7b38cef99cfa6129244b2fcd
Kuba Mazurek <zakaprov@gmail.com>
2021-04-19 16:54:52 +0200

    Add payments integration tests

---
+++ goth_tests/domain/payments/test_payment_driver_list_cmd.py
@@ -0,0 +1,61 @@
+"""Tests payment driver list CLI command."""
+
+import logging
+from pathlib import Path
+from typing import List, Optional
+
+import pytest
+
+from goth.address import (
+    PROXY_HOST,
+    YAGNA_REST_URL,
+)
+from goth.configuration import load_yaml, Override
+from goth.runner import Runner
+from goth.runner.probe import RequestorProbe
+
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.asyncio
+async def test_payment_driver_list(
+    common_assets: Path,
+    config_overrides: Optional[List[Override]],
+    log_dir: Path,
+):
+    """Test just the requestor's CLI command, no need to setup provider."""
+
+    nodes = [
+        {"name": "requestor", "type": "Requestor"},
+    ]
+    config_overrides = config_overrides or []
+    config_overrides.append(("nodes", nodes))
+
+    goth_config = load_yaml(common_assets / "goth-config.yml", config_overrides)
+    task_package = (
+        "hash://sha3:d5e31b2eed628572a5898bf8c34447644bfc4b5130cfc1e4f10aeaa1:"
+        "http://3.249.139.167:8000/rust-wasi-tutorial.zip"
+    )
+
+    runner = Runner(
+        base_log_dir=log_dir,
+        compose_config=goth_config.compose_config,
+        web_root_path=common_assets / "web-root",
+    )
+
+    async with runner(goth_config.containers):
+        requestor = runner.get_probes(probe_type=RequestorProbe)[0]
+        res = requestor.cli.payment_drivers()
+        assert res and res.items()
+        driver = next(iter(res.values()), None)
+
+        assert driver
+        assert driver.default_network, "Default network should be set"
+
+        network = driver.networks.get(driver.default_network, None)
+        assert network, "Network should belong to the Driver"
+        assert network.default_token, "Default taken should be set"
+
+        token = network.tokens.get(network.default_token, None)
+        assert token, "Token should belong to the Network"
--- goth_tests/domain/payments/test_zero_amount_txs.py
+++ goth_tests/domain/payments/test_zero_amount_txs.py
@@ -1,6 +1,5 @@
-"""End to end tests for requesting WASM tasks using goth REST API clients."""
+"""Tests that zero-amount invoices are settled."""
 
-import json
 import logging
 from pathlib import Path
 from typing import List, Optional
@@ -15,20 +14,28 @@ from goth.configuration import load_yaml, Override
 from goth.node import node_environment
 from goth.runner import Runner
 from goth.runner.probe import ProviderProbe, RequestorProbe
+from ya_payment import InvoiceStatus
 
 from goth_tests.helpers.activity import wasi_exe_script
 from goth_tests.helpers.negotiation import DemandBuilder, negotiate_agreements
 
-logger = logging.getLogger("goth.test.e2e_wasi")
+logger = logging.getLogger("goth.test.zero_amount_txs")
 
 
 @pytest.mark.asyncio
-async def test_e2e_wasi(
+async def test_zero_amount_invoice(
     common_assets: Path,
     config_overrides: Optional[List[Override]],
     log_dir: Path,
 ):
     """Test successful flow requesting WASM tasks with goth REST API client."""
+
+    nodes = [
+        {"name": "requestor", "type": "Requestor"},
+        {"name": "provider-1", "type": "VM-Wasm-Provider", "use-proxy": True},
+    ]
+    config_overrides = config_overrides or []
+    config_overrides.append(("nodes", nodes))
 
     goth_config = load_yaml(common_assets / "goth-config.yml", config_overrides)
     task_package = (
@@ -44,7 +51,7 @@ async def test_e2e_wasi(
 
     async with runner(goth_config.containers):
         requestor = runner.get_probes(probe_type=RequestorProbe)[0]
-        providers = runner.get_probes(probe_type=ProviderProbe)
+        provider = runner.get_probes(probe_type=ProviderProbe)[0]
 
         # Market
         demand = DemandBuilder(requestor).props_from_template(task_package).build()
@@ -52,30 +59,24 @@ async def test_e2e_wasi(
         agreement_providers = await negotiate_agreements(
             requestor,
             demand,
-            providers,
+            [provider],
             lambda p: p.properties.get("golem.runtime.name") == "wasmtime",
         )
+        agreement_id = agreement_providers[0][0]
 
-        # Activity
-        exe_script = wasi_exe_script(runner)
-        num_commands = len(exe_script)
-
-        for agreement_id, provider in agreement_providers:
-            logger.info("Running activity on %s", provider.name)
-            activity_id = await requestor.create_activity(agreement_id)
-            await provider.wait_for_exeunit_started()
-            batch_id = await requestor.call_exec(activity_id, json.dumps(exe_script))
-            await requestor.collect_results(
-                activity_id, batch_id, num_commands, timeout=30
-            )
-            await requestor.destroy_activity(activity_id)
-            await provider.wait_for_exeunit_finished()
+        #  Zero-amount invoice is issued when agreement is terminated
+        #  without activity
+        await requestor.wait_for_approval(agreement_id)
+        await requestor.terminate_agreement(agreement_id, None)
 
         # Payment
-        for agreement_id, provider in agreement_providers:
-            await provider.wait_for_invoice_sent()
-            invoices = await requestor.gather_invoices(agreement_id)
-            assert all(inv.agreement_id == agreement_id for inv in invoices)
-            # TODO:
-            await requestor.pay_invoices(invoices)
-            await provider.wait_for_invoice_paid()
+
+        await provider.wait_for_invoice_sent()
+        invoices = await requestor.gather_invoices(agreement_id)
+        await requestor.pay_invoices(invoices)
+        await provider.wait_for_invoice_paid()
+
+        # verify requestor's invoice is settled
+        invoice = (await requestor.gather_invoices(agreement_id))[0]
+        assert invoice.amount == "0"
+        assert invoice.status == InvoiceStatus.SETTLED
