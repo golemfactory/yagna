b2cf0e9abd5010843770932850c5c107ba998df3
Dariusz Rybi <jiivan@users.noreply.github.com>
2021-04-30 16:25:07 +0200

    Additional market broadcast metrics (#1288)
    * Additional market broadcast metrics
    
    Co-authored-by: Piotr Chromiec <tworec@golem.network>

--- core/market/src/matcher.rs
+++ core/market/src/matcher.rs
@@ -76,8 +76,14 @@ impl Matcher {
         // until first change to value will be made.
         counter!("market.offers.incoming", 0);
         counter!("market.offers.broadcasts", 0);
+        counter!("market.offers.broadcasts.size", 0);
+        counter!("market.offers.broadcasts.net", 0);
+        counter!("market.offers.broadcasts.net_errors", 0);
         counter!("market.offers.unsubscribes.incoming", 0);
         counter!("market.offers.unsubscribes.broadcasts", 0);
+        counter!("market.offers.unsubscribes.broadcasts.size", 0);
+        counter!("market.offers.unsubscribes.broadcasts.net", 0);
+        counter!("market.offers.unsubscribes.broadcasts.net_errors", 0);
 
         Ok((matcher, listeners))
     }
