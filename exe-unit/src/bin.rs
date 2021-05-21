ad67996b6d38bf248db075e88f14328d463007f0
mfranciszkiewicz <marek@golem.network>
2021-05-21 15:45:35 +0200

    ExeUnit start & teardown fix

--- exe-unit/src/bin.rs
+++ exe-unit/src/bin.rs
@@ -1,5 +1,6 @@
-use actix::{Actor, Addr, System};
+use actix::{Actor, Addr};
 use anyhow::{bail, Context};
+use futures::channel::oneshot;
 use std::convert::TryFrom;
 use std::path::PathBuf;
 use structopt::{clap, StructOpt};
@@ -184,7 +185,7 @@ async fn send_script(
     }
 }
 
-fn run() -> anyhow::Result<()> {
+async fn run() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
 
     #[allow(unused_mut)]
@@ -293,30 +294,31 @@ fn run() -> anyhow::Result<()> {
     log::debug!("CLI args: {:?}", cli);
     log::debug!("ExeUnitContext args: {:?}", ctx);
 
-    let sys = System::new();
+    let (tx, rx) = oneshot::channel();
 
     let metrics = MetricsService::try_new(&ctx, Some(10000), ctx.supervise.hardware)?.start();
     let transfers = TransferService::new(&ctx).start();
     let runtime = RuntimeProcess::new(&ctx, cli.binary).start();
-    let exe_unit = ExeUnit::new(ctx, metrics, transfers, runtime).start();
+    let exe_unit = ExeUnit::new(tx, ctx, metrics, transfers, runtime).start();
     let signals = SignalMonitor::new(exe_unit.clone()).start();
-    exe_unit.do_send(Register(signals));
+    exe_unit.send(Register(signals)).await?;
 
     if let Some(exe_script) = commands {
         tokio::task::spawn(send_script(exe_unit, ctx_activity_id, exe_script));
     }
 
-    sys.run()?;
+    rx.await??;
     Ok(())
 }
 
-fn main() {
+#[actix_rt::main]
+async fn main() {
     if let Err(error) = start_file_logger() {
         start_logger().expect("Failed to start logging");
         log::warn!("Using fallback logging due to an error: {:?}", error);
     };
 
-    std::process::exit(match run() {
+    std::process::exit(match run().await {
         Ok(_) => 0,
         Err(error) => {
             log::error!("{}", error);
