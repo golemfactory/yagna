00946a565b4cdb8d9459f28fcaf65c9c5152d2b5
Kuba Mazurek <jakub.mazurek@golem.network>
2021-06-29 21:04:59 +0200

    Move goth log assertions to yagna repo (#1448)

--- goth_tests/domain/payments/test_payment_driver_list_cmd.py
+++ goth_tests/domain/payments/test_payment_driver_list_cmd.py
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 @pytest.mark.asyncio
 async def test_payment_driver_list(
     common_assets: Path,
+    default_config: Path,
     config_overrides: List[Override],
     log_dir: Path,
 ):
@@ -30,7 +31,7 @@ async def test_payment_driver_list(
         {"name": "requestor", "type": "Requestor"},
     ]
     config_overrides.append(("nodes", nodes))
-    goth_config = load_yaml(common_assets / "goth-config.yml", config_overrides)
+    goth_config = load_yaml(default_config, config_overrides)
 
     runner = Runner(
         base_log_dir=log_dir,
