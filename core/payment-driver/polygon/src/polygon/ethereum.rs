51ef3ed8f70f70340615daa49d269fffa08a4f4d
scx1332 <sieciech.czajka@golem.network>
2021-09-29 18:12:59 +0200

    Set limits for minimum and maximum gas prices on polygon driver. Use env variables. GLM_POLYGON_MAX_GAS_PRICE (default 1000000000000) - 1000Gwei and GLM_POLYGON_MIN_GAS_PRICE (default 6100000000) - 6.1 GWei

--- core/payment-driver/polygon/src/polygon/ethereum.rs
+++ core/payment-driver/polygon/src/polygon/ethereum.rs
@@ -23,6 +23,12 @@ lazy_static! {
             Ok(Ok(x)) => x,
             _ => 1000000000000, //1000 Gwei
         };
+    pub static ref GLM_POLYGON_MIN_GAS_PRICE: u64 =
+        match env::var("GLM_POLYGON_MIN_GAS_PRICE").map(|s| s.parse()) {
+            Ok(Ok(x)) => x,
+            _ => 6100000000, //6.1 Gwei
+        };
+
 }
 const CREATE_FAUCET_FUNCTION: &str = "create";
 const BALANCE_ERC20_FUNCTION: &str = "balanceOf";
@@ -128,14 +134,17 @@ pub async fn sign_transfer_tx(
 
     let data = eth_utils::contract_encode(&contract, TRANSFER_ERC20_FUNCTION, (recipient, amount))
         .map_err(GenericError::new)?;
-    let gas_price = client.eth().gas_price().await.map_err(GenericError::new)?;
+    let mut gas_price = client.eth().gas_price().await.map_err(GenericError::new)?;
 
     if gas_price > U256::from(*GLM_POLYGON_MAX_GAS_PRICE) {
-        return Err(GenericError::new(format!(
-            "Gas priced exceeded max set value! gasPrice: {}. maximumGasPrice: {}",
-            gas_price, *GLM_POLYGON_MAX_GAS_PRICE
-        )));
+        log::warn!("Gas price higher than maximum {}/{}. Continuing with lower gas price...", gas_price, *GLM_POLYGON_MAX_GAS_PRICE);
+        gas_price = U256::from(*GLM_POLYGON_MAX_GAS_PRICE);
     };
+
+    if gas_price < U256::from(*GLM_POLYGON_MIN_GAS_PRICE) {
+        log::info!("Gas price lower than mininimum {}/{}. Continuing with higher gas price...", gas_price, *GLM_POLYGON_MIN_GAS_PRICE);
+        gas_price = U256::from(*GLM_POLYGON_MIN_GAS_PRICE);
+    }
 
     let tx = RawTransaction {
         nonce,
