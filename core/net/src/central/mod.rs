66042d670ffc949d47a6f5f9e41080e6827c195a
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2021-11-19 15:52:37 +0100

    Restore service feature

--- core/net/src/central/mod.rs
+++ core/net/src/central/mod.rs
@@ -1,12 +1,16 @@
 mod api;
+#[cfg(any(feature = "service", test))]
 mod handler;
+#[cfg(any(feature = "service", test))]
 mod service;
 
 pub use api::*;
+#[cfg(any(feature = "service", test))]
 pub use service::{bind_remote, Net};
 
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
+
 use ya_core_model::net::local::Subscribe;
 
 lazy_static::lazy_static! {
