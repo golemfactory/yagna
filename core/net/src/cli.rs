5ad26a910cf9591fd4aaabb1123dc213440d2349
mfranciszkiewicz <marek@golem.network>
2022-03-24 14:11:43 +0100

    hybrid net inspection CLI

---
+++ core/net/src/cli.rs
@@ -0,0 +1,94 @@
+use humantime::format_duration;
+use std::cmp::Ordering;
+use std::time::Duration;
+use structopt::*;
+
+use ya_core_model::net::local as model;
+use ya_service_api::{CliCtx, CommandOutput, ResponseTable};
+use ya_service_bus::{typed as bus, RpcEndpoint};
+
+#[derive(StructOpt, Debug)]
+#[structopt(setting = clap::AppSettings::DeriveDisplayOrder)]
+/// Network management
+pub enum NetCommand {
+    /// Network status
+    Status {},
+    /// List network sessions
+    Sessions {},
+    /// List virtual sockets
+    Sockets {},
+}
+
+impl NetCommand {
+    pub async fn run_command(self, _ctx: &CliCtx) -> anyhow::Result<CommandOutput> {
+        match self {
+            NetCommand::Status {} => {
+                CommandOutput::object(bus::service(model::BUS_ID).send(model::Status {}).await??)
+            }
+            NetCommand::Sessions {} => {
+                let mut sessions: Vec<model::SessionResponse> = bus::service(model::BUS_ID)
+                    .send(model::Sessions {})
+                    .await
+                    .map_err(|e| anyhow::Error::msg(e))??;
+
+                sessions.sort_by_key(|s| s.node_id.unwrap_or_default().into_array());
+
+                Ok(ResponseTable {
+                    columns: vec![
+                        "node".into(),
+                        "address".into(),
+                        "type".into(),
+                        "time".into(),
+                    ],
+                    values: sessions
+                        .into_iter()
+                        .map(|s| {
+                            let time = Duration::from_secs(s.duration.as_secs());
+                            serde_json::json! {[
+                                s.node_id.map(|id| id.to_string()).unwrap_or_default(),
+                                s.remote_address.to_string(),
+                                s.session_type,
+                                format_duration(time).to_string(),
+                            ]}
+                        })
+                        .collect(),
+                }
+                .into())
+            }
+            NetCommand::Sockets {} => {
+                let mut sockets: Vec<model::SocketResponse> = bus::service(model::BUS_ID)
+                    .send(model::Sockets {})
+                    .await
+                    .map_err(|e| anyhow::Error::msg(e))??;
+
+                sockets.sort_by(|l, r| match l.remote_addr.cmp(&r.remote_addr) {
+                    Ordering::Equal => l.remote_port.cmp(&r.remote_port),
+                    result => result,
+                });
+
+                Ok(ResponseTable {
+                    columns: vec![
+                        "protocol".into(),
+                        "state".into(),
+                        "".into(),
+                        "address".into(),
+                        "port".into(),
+                    ],
+                    values: sockets
+                        .into_iter()
+                        .map(|s| {
+                            serde_json::json! {[
+                                s.protocol,
+                                s.state,
+                                s.local_port,
+                                s.remote_addr,
+                                s.remote_port,
+                            ]}
+                        })
+                        .collect(),
+                }
+                .into())
+            }
+        }
+    }
+}
---
+++ core/net/src/hybrid/cli.rs
@@ -0,0 +1,68 @@
+use std::time::Instant;
+use ya_core_model::net::local as model;
+use ya_service_bus::typed as bus;
+
+use crate::hybrid::service::CLIENT;
+
+pub(crate) fn bind_service() {
+    let _ = bus::bind(model::BUS_ID, move |_: model::Status| async move {
+        let client = {
+            CLIENT.with(|c| c.borrow().clone()).ok_or_else(|| {
+                model::StatusError::RuntimeException("client not initialized".to_string())
+            })?
+        };
+
+        Ok(model::StatusResponse {
+            node_id: client.node_id(),
+            listen_address: client.bind_addr().await.ok(),
+            public_address: client.public_addr().await,
+            sessions: client.sessions().await.len(),
+        })
+    });
+    let _ = bus::bind(model::BUS_ID, move |_: model::Sessions| async move {
+        let client = CLIENT.with(|c| c.borrow().clone()).ok_or_else(|| {
+            model::StatusError::RuntimeException("client not initialized".to_string())
+        })?;
+
+        let mut responses = Vec::new();
+        for session in client.sessions().await {
+            let node_id = client.remote_id(&session.remote).await;
+            let kind = match node_id {
+                Some(id) => {
+                    let is_p2p = client.sessions.is_p2p(&id).await;
+                    is_p2p.then(|| "p2p").unwrap_or("relayed")
+                }
+                None => "relay",
+            };
+
+            responses.push(model::SessionResponse {
+                node_id,
+                id: session.id.to_string(),
+                session_type: kind.to_string(),
+                remote_address: session.remote,
+                duration: Instant::now() - session.created,
+            });
+        }
+
+        Ok(responses)
+    });
+    let _ = bus::bind(model::BUS_ID, move |_: model::Sockets| async move {
+        let client = CLIENT.with(|c| c.borrow().clone()).ok_or_else(|| {
+            model::StatusError::RuntimeException("client not initialized".to_string())
+        })?;
+
+        let sockets = client
+            .sockets()
+            .into_iter()
+            .map(|(desc, state)| model::SocketResponse {
+                protocol: desc.protocol.to_string(),
+                state: state.to_string(),
+                local_port: desc.local.port_repr(),
+                remote_addr: desc.remote.addr_repr(),
+                remote_port: desc.remote.port_repr(),
+            })
+            .collect();
+
+        Ok(sockets)
+    });
+}
