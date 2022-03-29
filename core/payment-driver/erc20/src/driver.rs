7e57e0c9e3d62a6e8697659638397bb09c60df59
scx1332 <sieciech.czajka@golem.network>
2022-03-29 14:57:37 +0200

    Fix unwrap

--- core/payment-driver/erc20/src/driver.rs
+++ core/payment-driver/erc20/src/driver.rs
@@ -244,13 +244,16 @@ impl PaymentDriverCron for Erc20Driver {
             Some(guard) => guard,
         };
         log::trace!("Running ERC-20 send-out job...");
-        for network_key in self.get_networks().keys() {
+        'outer: for network_key in self.get_networks().keys() {
             let network = Network::from_str(&network_key).unwrap();
             // Process payment rows
             for node_id in self.active_accounts.borrow().list_accounts() {
-                cron::process_payments_for_account(&self.dao, &node_id, network)
-                    .await
-                    .unwrap();
+                if let Err(e) =
+                    cron::process_payments_for_account(&self.dao, &node_id, network).await
+                {
+                    log::error!("cron::process_payments_for_account returned error: {:?}", e);
+                    continue 'outer;
+                };
             }
             // Process transaction rows
             cron::process_transactions(&self.dao, network).await;
