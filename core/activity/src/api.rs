30850af91c4a9c10f8394c04de9d7630a16b08a4
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2022-05-26 15:29:06 +0200

    Fix app_data HttpService parameters that should be wrapped in Data object

--- core/activity/src/api.rs
+++ core/activity/src/api.rs
@@ -1,10 +1,11 @@
+use actix_web::web::Data;
 use actix_web::Scope;
 use ya_persistence::executor::DbExecutor;
 use ya_service_api_web::scope::ExtendableScope;
 
 pub fn web_scope(db: &DbExecutor) -> Scope {
     actix_web::web::scope(crate::ACTIVITY_API_PATH)
-        .app_data(db.clone())
+        .app_data(Data::new(db.clone()))
         .extend(common::extend_web_scope)
         .extend(crate::provider::extend_web_scope)
         .extend(crate::requestor::control::extend_web_scope)
