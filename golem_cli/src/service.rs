0395648899e28a88051c8486e4783267bcb09717
Dariusz Rybi <dariusz@golem.network>
2022-06-21 16:00:09 +0200

    [review] Use exit codes outside of bash & sysexits.h

--- golem_cli/src/service.rs
+++ golem_cli/src/service.rs
@@ -162,11 +162,11 @@ pub async fn run(mut config: RunConfig) -> Result</*exit code*/ i32> {
 
     if let Err(e) = provider.abort().await {
         log::warn!("provider exited with: {:?}", e);
-        return Ok(130);
+        return Ok(11);
     }
     if let Err(e) = service.abort().await {
         log::warn!("service exited with: {:?}", e);
-        return Ok(131);
+        return Ok(12);
     }
     Ok(0)
 }
