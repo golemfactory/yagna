c4ea44d36f689eccbb633da6c0f9c02259e4416f
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2022-09-29 12:43:47 +0200

    Extract CSR to test

--- utils/manifest-utils/tests/certificate_expiration.rs
+++ utils/manifest-utils/tests/certificate_expiration.rs
@@ -18,15 +18,15 @@ fn accept_not_expired_certificate() {
         .unwrap();
 
     let (self_signed_cert, ca_key_pair) = create_self_signed_certificate(valid_not_after).unwrap();
-
-    let signed_cert = create_signed_certificate(&self_signed_cert, &ca_key_pair).unwrap();
-
     write_cert_to_file(
-        self_signed_cert,
+        &self_signed_cert,
         &test_cert_dir.path().join("self_signed.pem"),
     );
 
     let sut = Keystore::load(&test_cert_dir).unwrap();
+
+    let (req, csr_key_pair) = create_csr().unwrap();
+    let signed_cert = sign_csr(req, csr_key_pair, &self_signed_cert, ca_key_pair).unwrap();
 
     assert!(sut
         .verify_cert(base64::encode(signed_cert.to_pem().unwrap()))
@@ -42,15 +42,15 @@ fn not_accept_expired_certificate() {
         .unwrap();
 
     let (self_signed_cert, ca_key_pair) = create_self_signed_certificate(valid_not_after).unwrap();
-
-    let signed_cert = create_signed_certificate(&self_signed_cert, &ca_key_pair).unwrap();
-
     write_cert_to_file(
-        self_signed_cert,
+        &self_signed_cert,
         &test_cert_dir.path().join("self_signed.pem"),
     );
 
     let sut = Keystore::load(&test_cert_dir).unwrap();
+
+    let (req, csr_key_pair) = create_csr().unwrap();
+    let signed_cert = sign_csr(req, csr_key_pair, &self_signed_cert, ca_key_pair).unwrap();
 
     assert!(sut
         .verify_cert(base64::encode(signed_cert.to_pem().unwrap()))
@@ -67,7 +67,7 @@ mod utils {
     use openssl::bn::{BigNum, MsbOption};
     use openssl::error::ErrorStack;
     use openssl::hash::MessageDigest;
-    use openssl::pkey::{PKey, PKeyRef, Private};
+    use openssl::pkey::{PKey, Private};
     use openssl::rsa::Rsa;
     use openssl::x509::extension::{
         AuthorityKeyIdentifier, BasicConstraints, KeyUsage, SubjectAlternativeName,
@@ -75,7 +75,7 @@ mod utils {
     };
     use openssl::x509::{X509NameBuilder, X509Ref, X509Req, X509ReqBuilder, X509};
 
-    pub fn write_cert_to_file(cert: X509, file_path: &Path) {
+    pub fn write_cert_to_file(cert: &X509Ref, file_path: &Path) {
         let mut file = File::create(&file_path).unwrap();
         file.write_all(&cert.to_pem().unwrap()).unwrap();
     }
@@ -130,9 +130,12 @@ mod utils {
     }
 
     /// Make a X509 request with the given private key
-    fn mk_request(key_pair: &PKey<Private>) -> Result<X509Req, ErrorStack> {
+    pub fn create_csr() -> Result<(X509Req, PKey<Private>), ErrorStack> {
+        let rsa = Rsa::generate(2048)?;
+        let key_pair = PKey::from_rsa(rsa)?;
+
         let mut req_builder = X509ReqBuilder::new()?;
-        req_builder.set_pubkey(key_pair)?;
+        req_builder.set_pubkey(&key_pair)?;
 
         let mut x509_name = X509NameBuilder::new()?;
         x509_name.append_entry_by_text("C", "US")?;
@@ -142,20 +145,17 @@ mod utils {
         let x509_name = x509_name.build();
         req_builder.set_subject_name(&x509_name)?;
 
-        req_builder.sign(key_pair, MessageDigest::sha256())?;
+        req_builder.sign(&key_pair, MessageDigest::sha256())?;
         let req = req_builder.build();
-        Ok(req)
+        Ok((req, key_pair))
     }
 
-    pub fn create_signed_certificate(
-        ca_cert: &X509Ref,
-        ca_key_pair: &PKeyRef<Private>,
+    pub fn sign_csr(
+        csr: X509Req,
+        csr_keys: PKey<Private>,
+        signing_cert: &X509Ref,
+        signing_keys: PKey<Private>,
     ) -> Result<X509, ErrorStack> {
-        let rsa = Rsa::generate(2048)?;
-        let key_pair = PKey::from_rsa(rsa)?;
-
-        let req = mk_request(&key_pair)?;
-
         let mut cert_builder = X509::builder()?;
         cert_builder.set_version(2)?;
         let serial_number = {
@@ -164,9 +164,9 @@ mod utils {
             serial.to_asn1_integer()?
         };
         cert_builder.set_serial_number(&serial_number)?;
-        cert_builder.set_subject_name(req.subject_name())?;
-        cert_builder.set_issuer_name(ca_cert.subject_name())?;
-        cert_builder.set_pubkey(&key_pair)?;
+        cert_builder.set_subject_name(csr.subject_name())?;
+        cert_builder.set_issuer_name(signing_cert.subject_name())?;
+        cert_builder.set_pubkey(&csr_keys)?;
         let not_before = Asn1Time::days_from_now(0)?;
         cert_builder.set_not_before(&not_before)?;
         let not_after = Asn1Time::days_from_now(365)?;
@@ -183,23 +183,23 @@ mod utils {
                 .build()?,
         )?;
 
-        let subject_key_identifier =
-            SubjectKeyIdentifier::new().build(&cert_builder.x509v3_context(Some(ca_cert), None))?;
+        let subject_key_identifier = SubjectKeyIdentifier::new()
+            .build(&cert_builder.x509v3_context(Some(signing_cert), None))?;
         cert_builder.append_extension(subject_key_identifier)?;
 
         let auth_key_identifier = AuthorityKeyIdentifier::new()
             .keyid(false)
             .issuer(false)
-            .build(&cert_builder.x509v3_context(Some(ca_cert), None))?;
+            .build(&cert_builder.x509v3_context(Some(signing_cert), None))?;
         cert_builder.append_extension(auth_key_identifier)?;
 
         let subject_alt_name = SubjectAlternativeName::new()
             .dns("*.example.com")
             .dns("hello.com")
-            .build(&cert_builder.x509v3_context(Some(ca_cert), None))?;
+            .build(&cert_builder.x509v3_context(Some(signing_cert), None))?;
         cert_builder.append_extension(subject_alt_name)?;
 
-        cert_builder.sign(ca_key_pair, MessageDigest::sha256())?;
+        cert_builder.sign(&signing_keys, MessageDigest::sha256())?;
         let cert = cert_builder.build();
 
         Ok(cert)
