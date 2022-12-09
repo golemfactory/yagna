94241c17e668f299b1739df3637eeb35c0f3562a
Przemyslaw Walski <pwalski@users.noreply.github.com>
2022-12-09 15:40:39 +0100

    WebSocket API for listening to GSB messages

--- core/gsb-api/src/services.rs
+++ core/gsb-api/src/services.rs
@@ -1,17 +1,23 @@
 //! Provider side operations
 use std::vec;
 
-use actix_http::StatusCode;
-use actix_web::{web, Responder};
+use actix::{Actor, StreamHandler};
+use actix_http::{
+    ws::{CloseReason, ProtocolError},
+    StatusCode,
+};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
 use serde::{Deserialize, Serialize};
 
+use serde_json::json;
 use ya_persistence::executor::DbExecutor;
 use ya_service_api_web::middleware::Identity;
 
 pub const DEFAULT_SERVICES_TIMEOUT: f32 = 60.0;
 
 pub fn extend_web_scope(scope: actix_web::Scope) -> actix_web::Scope {
-    scope.service(post_services)
+    scope.service(post_services).service(delete_services)
 }
 
 #[actix_web::post("/services")]
@@ -21,15 +27,16 @@ async fn post_services(
     id: Identity,
 ) -> impl Responder {
     log::debug!("POST /services Body: {:?}", body);
-    let services = ServicesBody { 
-            listen: Some(ServicesListenBody {
-                on: "dummy".to_string(),
-                components: vec!["foo".to_string(), "bar".to_string()],
-                links: Some(ServicesLinksBody {
-                    messages: "gsb-api/v1/services/dummy".to_string(),
-                })
-            })
-        };
+    let services = ServicesBody {
+        // let body = body.
+        listen: Some(ServicesListenBody {
+            on: "dummy".to_string(),
+            components: vec!["foo".to_string(), "bar".to_string()],
+            links: Some(ServicesLinksBody {
+                messages: "gsb-api/v1/services/dummy/messages".to_string(),
+            }),
+        }),
+    };
     web::Json(services)
         .customize()
         .with_status(StatusCode::CREATED)
@@ -39,6 +46,52 @@ async fn post_services(
 async fn delete_services(path: web::Path<ServicesPath>, id: Identity) -> impl Responder {
     log::debug!("DELETE /services/{}", path.key);
     web::Json(())
+}
+
+struct WsMessagesActor {}
+
+impl Actor for WsMessagesActor {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<actix_http::ws::Message, ProtocolError>> for WsMessagesActor {
+    fn handle(
+        &mut self,
+        item: Result<actix_http::ws::Message, ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        match item {
+            Ok(msg) => ctx.text(
+                json!({
+                  "id": "myId",
+                  "component": "GetMetadata",
+                  "payload": "payload",
+                })
+                .to_string(),
+            ),
+            Err(cause) => ctx.close(Some(CloseReason {
+                code: ws::CloseCode::Error,
+                description: Some(
+                    json!({
+                        "cause": cause.to_string()
+                    })
+                    .to_string(),
+                ),
+            })),
+        };
+    }
+}
+
+#[actix_web::get("/services/{key}/messages")]
+async fn get_service_messages(
+    path: web::Path<ServicesPath>,
+    req: HttpRequest,
+    stream: web::Payload,
+    id: Identity
+) -> Result<HttpResponse, Error> {
+    let resp = ws::start(WsMessagesActor {}, &req, stream);
+    println!("{:?}", resp);
+    resp
 }
 
 #[derive(Deserialize)]
