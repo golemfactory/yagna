b4101d3eb78e0154b3c528b7411163b4590be61a
Wojciech Kargul <wojciech.kargul@gmail.com>
2023-01-16 15:06:23 +0100

    No payload test

--- agent/provider/tests/manifest_negotiator.rs
+++ agent/provider/tests/manifest_negotiator.rs
@@ -7,7 +7,7 @@ use std::io::Write;
 use std::path::PathBuf;
 
 use pretty_assertions::assert_eq;
-use serde_json::json;
+use serde_json::{json, Value};
 use test_case::test_case;
 use ya_agreement_utils::AgreementView;
 use ya_manifest_test_utils::{load_certificates_from_dir, TestResources};
@@ -260,6 +260,53 @@ fn manifest_negotiator_test_with_invalid_signature(
     )
 }
 
+#[test_case(
+    r#"{"outbound": {"enabled": false, "everyone": "none", "audited-payload": {"default": {"mode": "none", "description": "default setting"}}}}"#, // rulestore config
+    r#"{ "patterns": [] }"#, // data_dir/domain_whitelist.json
+    None; // error msg
+    "Manifest accepted because no payload"
+)]
+#[serial]
+fn manifest_negotiator_test_no_payload(rulestore: &str, whitelist: &str, error_msg: Option<&str>) {
+    // Having
+    let (_, test_cert_dir) = MANIFEST_TEST_RESOURCES.init_cert_dirs();
+
+    let whitelist_file = create_whitelist_file(whitelist);
+    let rules_file_name = test_cert_dir.join("rules.json");
+    let mut rules_file = std::fs::File::create(&rules_file_name).unwrap();
+    rules_file.write_all(rulestore.as_bytes()).unwrap();
+
+    let rules_manager =
+        RulesManager::load_or_create(&rules_file_name, &whitelist_file, &test_cert_dir)
+            .expect("Can't load RulesManager");
+
+    let config = create_manifest_signature_validating_policy_config();
+    let negotiator_cfg = AgentNegotiatorsConfig { rules_manager };
+    let mut manifest_negotiator = ManifestSignature::new(&config, negotiator_cfg);
+    // Current implementation does not verify content of certificate permissions incoming in demand.
+
+    let demand = create_demand_json(None);
+    let demand = create_demand(demand);
+    let offer = create_offer();
+
+    // When
+    let negotiation_result = manifest_negotiator.negotiate_step(&demand, offer.clone());
+
+    // Then
+    let negotiation_result = negotiation_result.expect("Negotiator had not failed");
+    if let Some(message) = error_msg {
+        assert_eq!(
+            negotiation_result,
+            NegotiationResult::Reject {
+                message: message.to_string(),
+                is_final: true
+            }
+        );
+    } else {
+        assert_eq!(negotiation_result, NegotiationResult::Ready { offer });
+    }
+}
+
 #[allow(clippy::too_many_arguments)]
 fn manifest_negotiator_test_encoded_manifest_sign_and_cert_and_cert_dir_files(
     rulestore: &str,
@@ -299,21 +346,15 @@ fn manifest_negotiator_test_encoded_manifest_sign_and_cert_and_cert_dir_files(
     let mut manifest_negotiator = ManifestSignature::new(&config, negotiator_cfg);
     // Current implementation does not verify content of certificate permissions incoming in demand.
 
-    let demand = create_demand_json(
-        &comp_manifest_b64,
+    let demand = create_demand_json(Some(Payload {
+        comp_manifest_b64,
         signature_b64,
-        signature_alg,
+        signature_alg_b64: signature_alg,
         cert_b64,
         cert_permissions_b64,
-    );
-    let demand = AgreementView {
-        json: demand,
-        agreement_id: "id".to_string(),
-    };
-    let offer = AgreementView {
-        json: serde_json::from_str(r#"{ "any": "thing" }"#).unwrap(),
-        agreement_id: "id".to_string(),
-    };
+    }));
+    let demand = create_demand(demand);
+    let offer = create_offer();
 
     // When
     let negotiation_result = manifest_negotiator.negotiate_step(&demand, offer.clone());
@@ -330,6 +371,19 @@ fn manifest_negotiator_test_encoded_manifest_sign_and_cert_and_cert_dir_files(
         );
     } else {
         assert_eq!(negotiation_result, NegotiationResult::Ready { offer });
+    }
+}
+fn create_demand(demand: Value) -> AgreementView {
+    AgreementView {
+        json: demand,
+        agreement_id: "id".to_string(),
+    }
+}
+
+fn create_offer() -> AgreementView {
+    AgreementView {
+        json: serde_json::from_str(r#"{ "any": "thing" }"#).unwrap(),
+        agreement_id: "id".to_string(),
     }
 }
 
@@ -366,59 +420,133 @@ fn create_comp_manifest_b64(urls: &str) -> String {
     base64::encode(manifest)
 }
 
-fn create_demand_json(
-    comp_manifest_b64: &str,
+struct Payload<'a> {
+    comp_manifest_b64: String,
     signature_b64: Option<String>,
-    signature_alg_b64: Option<&str>,
+    signature_alg_b64: Option<&'a str>,
     cert_b64: Option<String>,
-    cert_permissions_b64: Option<&str>,
-) -> serde_json::Value {
-    let mut payload = HashMap::new();
-    payload.insert("@tag", json!(comp_manifest_b64));
-    if signature_b64.is_some() && signature_alg_b64.is_some() {
-        payload.insert(
-            "sig",
-            json!({
-                "@tag": signature_b64.unwrap(),
-                "algorithm": signature_alg_b64.unwrap().to_string()
-            }),
-        );
-    } else if signature_b64.is_some() {
-        payload.insert("sig", json!(signature_b64.unwrap()));
-    } else if signature_alg_b64.is_some() {
-        payload.insert(
-            "sig",
-            json!({ "algorithm": signature_alg_b64.unwrap().to_string() }),
-        );
-    }
+    cert_permissions_b64: Option<&'a str>,
+}
 
-    if cert_b64.is_some() && cert_permissions_b64.is_some() {
-        payload.insert(
-            "cert",
-            json!({
-                "@tag": cert_b64.unwrap(),
-                "permissions": cert_permissions_b64.unwrap().to_string()
-            }),
-        );
-    } else if let Some(cert_b64) = cert_b64 {
-        payload.insert("cert", json!(cert_b64));
-    }
+// fn create_demand_json(
+//     comp_manifest_b64: &str,
+//     signature_b64: Option<String>,
+//     signature_alg_b64: Option<&str>,
+//     cert_b64: Option<String>,
+//     cert_permissions_b64: Option<&str>,
+// ) -> serde_json::Value {
+//     let mut payload = HashMap::new();
+//     payload.insert("@tag", json!(comp_manifest_b64));
+//     if signature_b64.is_some() && signature_alg_b64.is_some() {
+//         payload.insert(
+//             "sig",
+//             json!({
+//                 "@tag": signature_b64.unwrap(),
+//                 "algorithm": signature_alg_b64.unwrap().to_string()
+//             }),
+//         );
+//     } else if signature_b64.is_some() {
+//         payload.insert("sig", json!(signature_b64.unwrap()));
+//     } else if signature_alg_b64.is_some() {
+//         payload.insert(
+//             "sig",
+//             json!({ "algorithm": signature_alg_b64.unwrap().to_string() }),
+//         );
+//     }
+//
+//     if cert_b64.is_some() && cert_permissions_b64.is_some() {
+//         payload.insert(
+//             "cert",
+//             json!({
+//                 "@tag": cert_b64.unwrap(),
+//                 "permissions": cert_permissions_b64.unwrap().to_string()
+//             }),
+//         );
+//     } else if let Some(cert_b64) = cert_b64 {
+//         payload.insert("cert", json!(cert_b64));
+//     }
+//
+//     // let mut payload = manifest.to_string();
+//     let manifest = json!({
+//         "golem": {
+//             "srv": {
+//                 "comp": {
+//                     "payload": payload
+//                 }
+//             }
+//         },
+//     });
+//     println!(
+//         "Tested demand:\n{}",
+//         serde_json::to_string_pretty(&manifest).unwrap()
+//     );
+//     manifest
+// }
 
-    // let mut payload = manifest.to_string();
-    let manifest = json!({
-        "golem": {
-            "srv": {
-                "comp": {
-                    "payload": payload
-                }
+fn create_demand_json(payload: Option<Payload>) -> Value {
+    match payload {
+        Some(p) => {
+            let mut payload = HashMap::new();
+            payload.insert("@tag", json!(p.comp_manifest_b64));
+            if p.signature_b64.is_some() && p.signature_alg_b64.is_some() {
+                payload.insert(
+                    "sig",
+                    json!({
+                        "@tag": p.signature_b64.unwrap(),
+                        "algorithm": p.signature_alg_b64.unwrap().to_string()
+                    }),
+                );
+            } else if p.signature_b64.is_some() {
+                payload.insert("sig", json!(p.signature_b64.unwrap()));
+            } else if p.signature_alg_b64.is_some() {
+                payload.insert(
+                    "sig",
+                    json!({ "algorithm": p.signature_alg_b64.unwrap().to_string() }),
+                );
             }
-        },
-    });
-    println!(
-        "Tested demand:\n{}",
-        serde_json::to_string_pretty(&manifest).unwrap()
-    );
-    manifest
+
+            if p.cert_b64.is_some() && p.cert_permissions_b64.is_some() {
+                payload.insert(
+                    "cert",
+                    json!({
+                        "@tag": p.cert_b64.unwrap(),
+                        "permissions": p.cert_permissions_b64.unwrap().to_string()
+                    }),
+                );
+            } else if let Some(cert_b64) = p.cert_b64 {
+                payload.insert("cert", json!(cert_b64));
+            }
+
+            let manifest = json!({
+                "golem": {
+                    "srv": {
+                        "comp": {
+                            "payload": payload
+                        }
+                    }
+                },
+            });
+            println!(
+                "Tested demand:\n{}",
+                serde_json::to_string_pretty(&manifest).unwrap()
+            );
+            manifest
+        }
+        _ => {
+            let manifest = json!({
+                "golem": {
+                    "srv": {
+                        "comp":{}
+                    }
+                },
+            });
+            println!(
+                "Tested demand:\n{}",
+                serde_json::to_string_pretty(&manifest).unwrap()
+            );
+            manifest
+        }
+    }
 }
 
 fn create_manifest_signature_validating_policy_config() -> PolicyConfig {
