0dbb623b6dc47f093454f23a58d51be4f47a3ff4
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-02-03 14:42:39 +0100

    align to urls

--- agent/provider/src/rules.rs
+++ agent/provider/src/rules.rs
@@ -297,10 +297,7 @@ impl RulesManager {
                             .and_then(|out| out.urls.as_ref())
                         {
                             for requested_url in requested_urls {
-                                if permitted_urls
-                                    .contains(&requested_url.as_str().to_string())
-                                    .not()
-                                {
+                                if permitted_urls.contains(&requested_url).not() {
                                     return Err(anyhow!(
                                         "Partner rule forbidden url requested: {requested_url}"
                                     ));
