22ccb579ce1416e623ec04a8e89c8d92872d13bf
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-04-26 19:45:19 +0200

    ya-provider clean more tests

--- agent/provider/src/dir.rs
+++ agent/provider/src/dir.rs
@@ -50,6 +50,7 @@ fn clean_dir<P: AsRef<Path>>(dir: P, lifetime: Duration, dry_run: bool) -> u64 {
     let deadline = SystemTime::now() - lifetime;
 
     let total_bytes = WalkDir::new(dir.as_ref())
+        .min_depth(1)
         .into_iter()
         .filter_map(|result| result.ok())
         .filter_map(|entry| match entry.metadata() {
@@ -91,41 +92,133 @@ fn clean_dir<P: AsRef<Path>>(dir: P, lifetime: Duration, dry_run: bool) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, path::PathBuf};
-    use crate::startup_config::{HARDWARE_JSON, PRESETS_JSON, GLOBALS_JSON};
-
     use super::clean_provider_dir;
+    use crate::startup_config::{GLOBALS_JSON, HARDWARE_JSON, PRESETS_JSON};
+    use std::{fs::File, io::Write, path::PathBuf};
 
     #[test]
     fn test_empty_dir_fail() {
         let dir = tempfile::tempdir().unwrap().into_path();
         let expected = anyhow::anyhow!("Not a provider data directory: {}", dir.display());
-        let actual = clean_provider_dir(dir, "1d", true, false);
-        assert_eq!(expected.to_string(), actual.err().unwrap().to_string());
+        let error = clean_provider_dir(&dir, "1d", true, false);
+        assert_eq!(expected.to_string(), error.err().unwrap().to_string());
+        assert!(dir.exists());
     }
 
     #[test]
-    fn test_data_dir_cleanup() {
-        let data_dir = create_data_dir(); 
-        let work_dir = crate::execution::exe_unit_work_dir(&data_dir);
-        std::fs::create_dir(work_dir);
-        let cache_dir = crate::execution::exe_unit_cache_dir(&data_dir);
-        std::fs::create_dir(cache_dir);
+    fn test_empty_exe_unit_dir_cleanup() {
+        let dirs = create_data_dir_w_exe_unit();
+        let removed_bytes =
+            clean_provider_dir(dirs.data_dir.dir, "1d", true, false).expect("Is ok");
 
-        let actual = clean_provider_dir(data_dir, "1d", true, false).expect("Is ok and returns 0");
-        assert_eq!(0, actual);
+        assert_eq!(0, removed_bytes);
+        assert!(dirs.data_dir.globals_json.exists());
+        assert!(dirs.data_dir.hardware_json.exists());
+        assert!(dirs.data_dir.presets_json.exists());
+        assert!(dirs.work_dir.exists());
+        assert!(dirs.cache_dir.exists());
     }
 
-    fn create_data_dir() -> PathBuf {
-        let data_dir = tempfile::tempdir().unwrap().into_path();
-        create_file(&data_dir, HARDWARE_JSON);
-        create_file(&data_dir, PRESETS_JSON);
-        create_file(&data_dir, GLOBALS_JSON); 
-        data_dir
+    #[test]
+    fn test_exe_unit_dir_cleanup_of_old_files() {
+        let dirs = create_data_dir_w_exe_unit();
+        let work_dir_file = create_file(&dirs.work_dir, "a.txt", "a");
+        let work_dir_dir = create_dir(&dirs.work_dir, "a");
+        let cache_dir_file = create_file(&dirs.cache_dir, "b.txt", "b");
+        let cache_dir_dir = create_dir(&dirs.cache_dir, "b");
+        let removed_bytes: u64 =
+            clean_provider_dir(dirs.data_dir.dir, "0us", true, false).expect("Is ok");
+
+        assert_eq!(2, removed_bytes);
+        assert!(dirs.data_dir.globals_json.exists());
+        assert!(dirs.data_dir.hardware_json.exists());
+        assert!(dirs.data_dir.presets_json.exists());
+        assert!(dirs.work_dir.exists());
+        assert!(dirs.cache_dir.exists());
+        assert!(!work_dir_dir.exists());
+        assert!(!work_dir_file.exists());
+        assert!(!cache_dir_dir.exists());
+        assert!(!cache_dir_file.exists());
     }
 
-    fn create_file(dir: &PathBuf, file_name: &str) {
-        let file_path = dir.join(file_name);
-        File::create(file_path).unwrap();
+    #[test]
+    fn test_exe_unit_dir_cleanup_does_not_remove_files() {
+        let dirs = create_data_dir_w_exe_unit();
+        let work_dir_file = create_file(&dirs.work_dir, "a.txt", "a");
+        let work_dir_dir = create_dir(&dirs.work_dir, "a");
+        let cache_dir_file = create_file(&dirs.cache_dir, "b.txt", "b");
+        let cache_dir_dir = create_dir(&dirs.cache_dir, "b");
+        let cache_dir_dir_file = create_file(&cache_dir_dir, "c.txt", "c");
+        let removed_bytes: u64 =
+            clean_provider_dir(dirs.data_dir.dir, "1h", true, false).expect("Is ok");
+
+        assert_eq!(0, removed_bytes);
+        assert!(dirs.data_dir.globals_json.exists());
+        assert!(dirs.data_dir.hardware_json.exists());
+        assert!(dirs.data_dir.presets_json.exists());
+        assert!(dirs.work_dir.exists());
+        assert!(dirs.cache_dir.exists());
+        assert!(
+            !work_dir_dir.exists(),
+            "Empty directories removed even when not expired"
+        );
+        assert!(work_dir_file.exists());
+        assert!(cache_dir_dir.exists());
+        assert!(cache_dir_file.exists());
+        assert!(cache_dir_dir_file.exists());
+    }
+
+    struct DataDir {
+        dir: PathBuf,
+        hardware_json: PathBuf,
+        presets_json: PathBuf,
+        globals_json: PathBuf,
+    }
+
+    struct ExeUnitDirs {
+        data_dir: DataDir,
+        cache_dir: PathBuf,
+        work_dir: PathBuf,
+    }
+
+    fn create_data_dir_w_exe_unit() -> ExeUnitDirs {
+        let data_dir = create_data_dir();
+        let work_dir = crate::execution::exe_unit_work_dir(&data_dir.dir);
+        let _ = std::fs::create_dir_all(&work_dir).unwrap();
+        let cache_dir = crate::execution::exe_unit_cache_dir(&data_dir.dir);
+        let _ = std::fs::create_dir_all(&cache_dir).unwrap();
+        ExeUnitDirs {
+            data_dir,
+            cache_dir,
+            work_dir,
+        }
+    }
+
+    fn create_data_dir() -> DataDir {
+        let dir = tempfile::tempdir().unwrap().into_path();
+        let hardware_json = create_file(&dir, HARDWARE_JSON, "a");
+        let presets_json = create_file(&dir, PRESETS_JSON, "b");
+        let globals_json = create_file(&dir, GLOBALS_JSON, "c");
+        DataDir {
+            dir,
+            hardware_json,
+            presets_json,
+            globals_json,
+        }
+    }
+
+    fn create_dir(dir: &PathBuf, name: &str) -> PathBuf {
+        let path = dir.join(name);
+        std::fs::create_dir_all(&path).unwrap();
+        path
+    }
+
+    fn create_file(dir: &PathBuf, name: &str, content: &str) -> PathBuf {
+        let path = dir.join(name);
+        let mut file = File::create(&path).unwrap();
+        if !content.is_empty() {
+            file.write_all(content.as_bytes()).unwrap();
+        }
+        path
     }
 }
