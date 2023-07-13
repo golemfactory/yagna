9f3fdf2d513b923c2fe9e3a2180ac764429c35af
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2023-07-13 17:05:24 +0200

    do not migrate for now

--- core/payment-driver/base/migrations/2023-07-10-rinkeby-cleanup/up.sql
+++
@@ -1,7 +0,0 @@
-
-ALTER TABLE `transaction` RENAME COLUMN network TO network_old;
-ALTER TABLE `transaction` ADD COLUMN network INTEGER NOT NULL DEFAULT 5;  -- 5 is goerli's network ID
-
-UPDATE `transaction` SET network = network_old;
-
-ALTER TABLE `transaction` DROP COLUMN network_old;
