4756187c87c5bc8f243df178305469b03be3bcb5
Kamil Koczurek <koczurekk@gmail.com>
2023-09-04 12:50:06 +0200

    erc20next: proper env handling

--- core/payment-driver/erc20next/src/service.rs
+++ core/payment-driver/erc20next/src/service.rs
@@ -48,41 +48,63 @@ impl Erc20NextService {
                 contract_use_direct_method: false,
                 contract_use_unpacked_method: false,
             };
+
             log::warn!("Loading config");
-            let config_str = include_str!("../config-payments.toml");
-            let mut config = match config::Config::load(path.join("config-payments.toml")).await {
-                Ok(config) => config,
-                Err(err) => {
-                    log::warn!(
-                        "Failed to load config from config-payments.toml due to {err:?}, using default config"
-                    );
-                    config::Config::load_from_str(config_str).unwrap()
+            let mut config = config::Config::load_from_str(include_str!("../config-payments.toml"))
+                .expect("Default erc20next config doesn't parse");
+
+            for (network, chain) in &mut config.chain {
+                let prefix = network.to_ascii_uppercase();
+                let Some(token) = &mut chain.token else { continue };
+                let symbol = token.symbol.to_ascii_uppercase();
+
+                let rpc_env = format!("{prefix}_GETH_ADDR");
+                let priority_fee_env = format!("{prefix}_PRIORITY_FEE");
+                let max_fee_per_gas_env = format!("{prefix}_MAX_FEE_PER_GAS");
+                let token_addr_env = format!("{prefix}_{symbol}_CONTRACT_ADDRESS");
+
+                if let Ok(addr) = env::var(&rpc_env) {
+                    chain.rpc_endpoints = addr.split(",").map(ToOwned::to_owned).collect();
+                    log::info!(
+                        "{} rpc endpoints set to {:?}",
+                        network,
+                        &chain.rpc_endpoints
+                    )
                 }
-            };
-
-            let env_overrides = [
-                ("goerli", "GOERLI_GETH_ADDR", "GOERLI_TGLM_CONTRACT_ADDRESS"),
-                (
-                    "polygon",
-                    "POLYGON_GETH_ADDR",
-                    "POLYGON_GLM_CONTRACT_ADDRESS",
-                ),
-            ];
-
-            for env_override in env_overrides {
-                log::info!("Checking overrides for {}", env_override.0);
-                if let Some(goerli_conf) = config.chain.get_mut(env_override.0) {
-                    if let Ok(addr) = env::var(env_override.1) {
-                        log::info!("Geth addr: {addr}");
-
-                        goerli_conf.rpc_endpoints = vec![addr];
+                if let Ok(fee) = env::var(&priority_fee_env) {
+                    match fee.parse::<f64>() {
+                        Ok(fee) => {
+                            log::info!("{network} priority fee set to {fee}");
+                            chain.priority_fee = fee;
+                        }
+                        Err(e) => log::warn!(
+                            "Valiue {fee} for {priority_fee_env} is not a valid devimal: {e}"
+                        ),
                     }
-                    if let Ok(addr) = env::var(env_override.2) {
-                        log::info!("Token addr: {addr}");
-
-                        goerli_conf.token.as_mut().unwrap().address =
-                            H160::from_str(&addr).unwrap();
+                }
+                if let Ok(max_fee) = env::var(&max_fee_per_gas_env) {
+                    match max_fee.parse::<f64>() {
+                        Ok(max_fee) => {
+                            log::info!("{network} max fee per gas set to {max_fee}");
+                            chain.max_fee_per_gas = max_fee;
+                        }
+                        Err(e) => log::warn!(
+                            "Valiue {max_fee} for {max_fee_per_gas_env} is not a valid devimal: {e}"
+                        ),
                     }
+                }
+                if let Ok(addr) = env::var(&token_addr_env) {
+                    match H160::from_str(&addr) {
+                        Ok(parsed) => {
+                            log::info!("{network} token address set to {addr}");
+                            token.address = parsed;
+                        }
+                        Err(e) => {
+                            log::warn!(
+                                "Value {addr} for {token_addr_env} is not valid H160 address: {e}"
+                            );
+                        }
+                    };
                 }
             }
 
