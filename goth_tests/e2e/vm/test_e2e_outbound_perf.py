31a50453b42f35226c5b6d95466275783e427698
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-10-06 15:59:59 +0200

    Revert "Goth test. Outbound perf + unresponsive requestor tests disabled"
    This reverts commit 09bc6f7cc08c5da18f01f716d1b1837811fca212.

--- goth_tests/e2e/vm/test_e2e_outbound_perf.py
+++ goth_tests/e2e/vm/test_e2e_outbound_perf.py
@@ -48,109 +48,109 @@ def vm_exe_script(runner: Runner, addr: str, output_file: str):
         },
     ]
 
-# @pytest.mark.asyncio
-# async def test_e2e_outbound_perf(
-#     common_assets: Path,
-#     default_config: Path,
-#     config_overrides: List[Override],
-#     log_dir: Path,
-# ):
-#     """Test successful flow requesting a task using outbound network feature. X.509 cert negotiation scenario."""
+@pytest.mark.asyncio
+async def test_e2e_outbound_perf(
+    common_assets: Path,
+    default_config: Path,
+    config_overrides: List[Override],
+    log_dir: Path,
+):
+    """Test successful flow requesting a task using outbound network feature. X.509 cert negotiation scenario."""
 
-#     # Test external api request just one Requestor and one Provider
-#     nodes = [
-#         {"name": "requestor", "type": "Requestor", "address": "d1d84f0e28d6fedf03c73151f98df95139700aa7" },
-#         {"name": "provider-1", "type": "VM-Wasm-Provider", "address": "63fc2ad3d021a4d7e64323529a55a9442c444da0", "use-proxy": True},
-#     ]
+    # Test external api request just one Requestor and one Provider
+    nodes = [
+        {"name": "requestor", "type": "Requestor", "address": "d1d84f0e28d6fedf03c73151f98df95139700aa7" },
+        {"name": "provider-1", "type": "VM-Wasm-Provider", "address": "63fc2ad3d021a4d7e64323529a55a9442c444da0", "use-proxy": True},
+    ]
 
-#     assets_root = Path(__file__).parent / "assets"
-#     node_types = [
-#        {"name": "Requestor", "class": "goth.runner.probe.RequestorProbe"},
-#        {
-#          "name": "VM-Wasm-Provider",
-#          "class": "goth_tests.helpers.probe.ProviderProbe",
-#          "mount": [
-#               {"read-only": "assets/provider/presets.json", "destination": "/root/.local/share/ya-provider/presets.json"},
-#               {"read-only": "assets/provider/hardware.json", "destination": "/root/.local/share/ya-provider/hardware.json"},
-#               {"read-write": f"{assets_root}/test_e2e_outbound_perf/provider/rules.json", "destination": "/root/.local/share/ya-provider/rules.json"},
-#          ],
-#          "privileged-mode": True,
-#        },
-#     ]
+    assets_root = Path(__file__).parent / "assets"
+    node_types = [
+       {"name": "Requestor", "class": "goth.runner.probe.RequestorProbe"},
+       {
+         "name": "VM-Wasm-Provider",
+         "class": "goth_tests.helpers.probe.ProviderProbe",
+         "mount": [
+              {"read-only": "assets/provider/presets.json", "destination": "/root/.local/share/ya-provider/presets.json"},
+              {"read-only": "assets/provider/hardware.json", "destination": "/root/.local/share/ya-provider/hardware.json"},
+              {"read-write": f"{assets_root}/test_e2e_outbound_perf/provider/rules.json", "destination": "/root/.local/share/ya-provider/rules.json"},
+         ],
+         "privileged-mode": True,
+       },
+    ]
 
-#     config_overrides.append(("nodes", nodes))
-#     config_overrides.append(("node-types", node_types))
+    config_overrides.append(("nodes", nodes))
+    config_overrides.append(("node-types", node_types))
 
-#     goth_config = load_yaml(default_config, config_overrides)
+    goth_config = load_yaml(default_config, config_overrides)
 
-#     runner = Runner(
-#         base_log_dir=log_dir,
-#         compose_config=goth_config.compose_config,
-#         web_root_path=Path(__file__).parent / "assets",
-#     )
+    runner = Runner(
+        base_log_dir=log_dir,
+        compose_config=goth_config.compose_config,
+        web_root_path=Path(__file__).parent / "assets",
+    )
 
-#     async with runner(goth_config.containers):
-#         server_addr = None
-#         for info in runner.get_container_info().values():
-#             if 'outbound-test' in info.aliases:
-#                 server_addr = info.address
-#                 break
-#         assert(server_addr is not None)  # "Can't find container `outbound-test`"
-#         logger.info("outbound-test container found at %s", server_addr)
+    async with runner(goth_config.containers):
+        server_addr = None
+        for info in runner.get_container_info().values():
+            if 'outbound-test' in info.aliases:
+                server_addr = info.address
+                break
+        assert(server_addr is not None)  # "Can't find container `outbound-test`"
+        logger.info("outbound-test container found at %s", server_addr)
 
-#         requestor = runner.get_probes(probe_type=RequestorProbe)[0]
-#         provider = runner.get_probes(probe_type=ProviderProbe)[0]
+        requestor = runner.get_probes(probe_type=RequestorProbe)[0]
+        provider = runner.get_probes(probe_type=ProviderProbe)[0]
 
-#         manifest = open(f"{runner.web_root_path}/test_e2e_outbound_perf/image/manifest.json").read()
+        manifest = open(f"{runner.web_root_path}/test_e2e_outbound_perf/image/manifest.json").read()
 
-#         # Market
-#         demand = (
-#             DemandBuilder(requestor)
-#             .props_from_template(task_package = None)
-#             .property("golem.srv.comp.payload", base64.b64encode(manifest.encode()).decode())
-#             .constraints("(&(golem.runtime.name=vm))")
-#             .build()
-#         )
+        # Market
+        demand = (
+            DemandBuilder(requestor)
+            .props_from_template(task_package = None)
+            .property("golem.srv.comp.payload", base64.b64encode(manifest.encode()).decode())
+            .constraints("(&(golem.runtime.name=vm))")
+            .build()
+        )
 
-#         agreement_providers = await negotiate_agreements(
-#             requestor,
-#             demand,
-#             [provider],
-#             lambda proposal: proposal.properties.get("golem.runtime.name") == "vm",
-#         )
+        agreement_providers = await negotiate_agreements(
+            requestor,
+            demand,
+            [provider],
+            lambda proposal: proposal.properties.get("golem.runtime.name") == "vm",
+        )
 
-#         agreement_id, provider = agreement_providers[0]
+        agreement_id, provider = agreement_providers[0]
 
-#         # Activity
+        # Activity
 
-#         output_file = "output.json"
-#         output_path = Path(runner.web_root_path) / "upload" / output_file
+        output_file = "output.json"
+        output_path = Path(runner.web_root_path) / "upload" / output_file
 
-#         exe_script = vm_exe_script(runner, server_addr, output_file)
-#         print(exe_script)
+        exe_script = vm_exe_script(runner, server_addr, output_file)
+        print(exe_script)
 
-#         num_commands = len(exe_script)
+        num_commands = len(exe_script)
 
-#         logger.info("Running activity on %s", provider.name)
-#         activity_id = await requestor.create_activity(agreement_id)
-#         await provider.wait_for_exeunit_started()
-#         batch_id = await requestor.call_exec(activity_id, json.dumps(exe_script))
-#         logger.info("Collecting %d results", num_commands)
-#         await requestor.collect_results(
-#             activity_id, batch_id, num_commands, timeout=300
-#         )
-#         logger.info("Collected results %d", num_commands)
-#         await requestor.destroy_activity(activity_id)
-#         await provider.wait_for_exeunit_finished()
+        logger.info("Running activity on %s", provider.name)
+        activity_id = await requestor.create_activity(agreement_id)
+        await provider.wait_for_exeunit_started()
+        batch_id = await requestor.call_exec(activity_id, json.dumps(exe_script))
+        logger.info("Collecting %d results", num_commands)
+        await requestor.collect_results(
+            activity_id, batch_id, num_commands, timeout=300
+        )
+        logger.info("Collected results %d", num_commands)
+        await requestor.destroy_activity(activity_id)
+        await provider.wait_for_exeunit_finished()
 
-#         assert output_path.is_file()
-#         assert len(output_path.read_text()) > 0
+        assert output_path.is_file()
+        assert len(output_path.read_text()) > 0
         
-#         output_text = open(output_path).read()
-#         output_json = json.loads(output_text)
+        output_text = open(output_path).read()
+        output_json = json.loads(output_text)
 
-#         pass_set = [{'Ok': True}, {'Err': 'skipped'}]
-#         assert output_json['roundtrip'] in pass_set
-#         assert output_json['iperf3'] in pass_set
-#         assert output_json['stress'] in pass_set
-#         assert output_json['many_reqs'] in pass_set
+        pass_set = [{'Ok': True}, {'Err': 'skipped'}]
+        assert output_json['roundtrip'] in pass_set
+        assert output_json['iperf3'] in pass_set
+        assert output_json['stress'] in pass_set
+        assert output_json['many_reqs'] in pass_set
