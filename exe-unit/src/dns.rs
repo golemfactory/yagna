7b480eaec758136fb4508eef4237b3c5753da90b
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-10-11 17:56:57 +0200

    Clippy. Fmt

--- exe-unit/src/dns.rs
+++ exe-unit/src/dns.rs
@@ -4,6 +4,9 @@ use std::str::FromStr;
 use trust_dns_resolver::config;
 use trust_dns_resolver::TokioAsyncResolver;
 
+#[cfg(test)]
+use std::time::Duration;
+
 #[derive(Clone)]
 pub struct StableResolver {
     stable_dns: IpAddr,
@@ -77,42 +80,34 @@ pub fn dns_servers() -> impl Iterator<Item = IpAddr> {
         .chain(QUAD9_IPS.iter().cloned())
 }
 
+// Do not use it in CI
 #[cfg(test)]
-mod tests {
-    use std::time::Duration;
-
-    use super::google_resolver;
-    use super::resolver;
-
-    // Do not use it in CI
-    #[cfg(test)]
-    #[ignore]
-    #[actix_rt::test]
-    async fn test_resolver() {
-        let name = "accounts.google.com";
-        let r = resolver().await.unwrap();
-        let ac = r.ips(name).await.unwrap();
-        for _i in 1..5 {
-            actix_rt::time::sleep(Duration::from_secs(30)).await;
-            let ac2 = r.ips(name).await.unwrap();
-            assert_eq!(ac, ac2);
-        }
+#[ignore]
+#[actix_rt::test]
+async fn test_resolver() {
+    let name = "accounts.google.com";
+    let r = resolver().await.unwrap();
+    let ac = r.ips(name).await.unwrap();
+    for _i in 1..5 {
+        actix_rt::time::sleep(Duration::from_secs(30)).await;
+        let ac2 = r.ips(name).await.unwrap();
+        assert_eq!(ac, ac2);
     }
+}
 
-    // Do not use it in CI
-    #[cfg(test)]
-    #[ignore]
-    #[should_panic]
-    #[actix_rt::test]
-    async fn test_fail_resolver() {
-        let name = "accounts.google.com";
-        let r = google_resolver().await.unwrap();
-        let ac = r.ips(name).await.unwrap();
-        for _i in 1..5 {
-            actix_rt::time::sleep(Duration::from_secs(15)).await;
-            r.clear_cache();
-            let ac2 = r.ips(name).await.unwrap();
-            assert_eq!(ac, ac2);
-        }
+// Do not use it in CI
+#[cfg(test)]
+#[ignore]
+#[should_panic]
+#[actix_rt::test]
+async fn test_fail_resolver() {
+    let name = "accounts.google.com";
+    let r = google_resolver().await.unwrap();
+    let ac = r.ips(name).await.unwrap();
+    for _i in 1..5 {
+        actix_rt::time::sleep(Duration::from_secs(15)).await;
+        r.clear_cache();
+        let ac2 = r.ips(name).await.unwrap();
+        assert_eq!(ac, ac2);
     }
 }
