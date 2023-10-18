0046c6c4ee4de33cfff391c1867c446d099f2b24
Kamil Koczurek <koczurekk@gmail.com>
2023-10-18 16:20:09 +0200

    payment: add indices to send_accept migrations

--- core/payment/migrations/2023-10-16-123456_send_accept/down.sql
+++ core/payment/migrations/2023-10-16-123456_send_accept/down.sql
@@ -2,4 +2,8 @@ ALTER TABLE pay_invoice REMOVE COLUMN send_accept;
 ALTER TABLE pay_debit_note REMOVE COLUMN send_accept;
 ALTER TABLE pay_payment REMOVE COLUMN send_payment;
 
+DROP INDEX pay_invoice_send_accept_idx;
+DROP INDEX pay_debit_note_send_accept_idx;
+DROP INDEX pay_payment_send_payment_idx;
+
 DROP TABLE pay_sync_needed_notifs;
--- core/payment/migrations/2023-10-16-123456_send_accept/up.sql
+++ core/payment/migrations/2023-10-16-123456_send_accept/up.sql
@@ -2,6 +2,10 @@ ALTER TABLE pay_invoice ADD COLUMN send_accept BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE pay_debit_note ADD COLUMN send_accept BOOLEAN NOT NULL DEFAULT FALSE;
 ALTER TABLE pay_payment ADD COLUMN send_payment BOOLEAN NOT NULL DEFAULT FALSE;
 
+CREATE INDEX pay_invoice_send_accept_idx ON pay_invoice (send_accept);
+CREATE INDEX pay_debit_note_send_accept_idx ON pay_invoice (send_accept);
+CREATE INDEX pay_payment_send_payment_idx ON pay_invoice (send_payment);
+
 CREATE TABLE pay_sync_needed_notifs(
     id VARCHAR(50) NOT NULL PRIMARY KEY,
     last_ping DATETIME NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
