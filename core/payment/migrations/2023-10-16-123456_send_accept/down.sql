fd2f2e07dee580f91571ac9e763e8d94421fc4dc
Kamil Koczurek <koczurekk@gmail.com>
2023-10-19 16:04:05 +0200

    payment: fix down migration sql

--- core/payment/migrations/2023-10-16-123456_send_accept/down.sql
+++ core/payment/migrations/2023-10-16-123456_send_accept/down.sql
@@ -1,9 +1,9 @@
-ALTER TABLE pay_invoice REMOVE COLUMN send_accept;
-ALTER TABLE pay_debit_note REMOVE COLUMN send_accept;
-ALTER TABLE pay_payment REMOVE COLUMN send_payment;
-
 DROP INDEX pay_invoice_send_accept_idx;
 DROP INDEX pay_debit_note_send_accept_idx;
 DROP INDEX pay_payment_send_payment_idx;
+
+ALTER TABLE pay_invoice REMOVE COLUMN send_accept;
+ALTER TABLE pay_debit_note REMOVE COLUMN send_accept;
+ALTER TABLE pay_payment REMOVE COLUMN send_payment;
 
 DROP TABLE pay_sync_needed_notifs;
