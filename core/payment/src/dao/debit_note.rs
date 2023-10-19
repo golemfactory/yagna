da8324deaa5844a14457e3ea45c744abd3f924dc
Kamil Koczurek <koczurekk@gmail.com>
2023-10-19 15:16:30 +0200

    payment: fix debit note dao unsent_accepted

--- core/payment/src/dao/debit_note.rs
+++ core/payment/src/dao/debit_note.rs
@@ -327,7 +327,11 @@ impl<'c> DebitNoteDao<'c> {
     /// Lists debit notes with send_accept
     pub async fn unsent_accepted(&self, owner_id: NodeId) -> DbResult<Vec<DebitNote>> {
         readonly_transaction(self.pool, move |conn| {
-            let read: Vec<ReadObj> = query!().order_by(dsl::timestamp.desc()).load(conn)?;
+            let read: Vec<ReadObj> = query!()
+                .filter(dsl::owner_id.eq(owner_id))
+                .filter(dsl::send_accept.eq(true))
+                .order_by(dsl::timestamp.desc())
+                .load(conn)?;
             let mut debit_notes = Vec::new();
             for obj in read {
                 debit_notes.push(obj.try_into()?);
