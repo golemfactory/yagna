041176b9b42122ff220f5c49907d40c2a353ac6c
Kamil Koczurek <koczurekk@gmail.com>
2024-01-09 16:31:18 +0100

    payment-driver: don't create DB for erc20next
    Also:
    * `db: DbExecutor` arguments in PaymentDriver methods were all dead code,
      so I removed them.

--- core/serv/src/main.rs
+++ core/serv/src/main.rs
@@ -289,8 +289,7 @@ async fn start_payment_drivers(data_dir: &Path) -> anyhow::Result<Vec<String>> {
     #[cfg(feature = "erc20next-driver")]
     {
         use ya_erc20next_driver::{PaymentDriverService, DRIVER_NAME};
-        let db_executor = DbExecutor::from_data_dir(data_dir, "erc20next-driver")?;
-        PaymentDriverService::gsb(&db_executor, data_dir.to_path_buf()).await?;
+        PaymentDriverService::gsb(data_dir.to_path_buf()).await?;
         drivers.push(DRIVER_NAME.to_owned());
     }
     Ok(drivers)
