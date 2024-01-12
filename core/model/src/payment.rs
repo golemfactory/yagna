9221892f442b68c2e91b0a9eaebb69d503961dd4
scx1332 <sieciech.czajka@golem.network>
2024-01-12 12:57:57 +0100

    f

--- core/model/src/payment.rs
+++ core/model/src/payment.rs
@@ -526,7 +526,7 @@ pub mod local {
         #[structopt(long, possible_values = DriverName::VARIANTS, default_value = DriverName::Erc20Next.into())]
         pub driver: DriverName,
         /// Payment network
-        #[structopt(long, possible_values = NetworkName::VARIANTS, default_value = NetworkName::Goerli.into())]
+        #[structopt(long, possible_values = NetworkName::VARIANTS, default_value = NetworkName::Holesky.into())]
         pub network: NetworkName,
     }
 
