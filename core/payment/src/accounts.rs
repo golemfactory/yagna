727efc370e6be55216653ab8999b72ea69a07b38
scx1332 <sieciech.czajka@golem.network>
2024-01-15 13:29:20 +0100

    More informative error

--- core/payment/src/accounts.rs
+++ core/payment/src/accounts.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use serde::{Deserialize, Serialize};
 use ya_core_model::driver::{driver_bus_id, AccountMode, Init};
 use ya_service_bus::typed as bus;
@@ -19,14 +20,30 @@ pub(crate) async fn init_account(account: Account) -> anyhow::Result<()> {
     let mut mode = AccountMode::NONE;
     mode.set(AccountMode::SEND, account.send);
     mode.set(AccountMode::RECV, account.receive);
-    bus::service(driver_bus_id(account.driver))
+    match bus::service(driver_bus_id(account.driver.clone()))
         .call(Init::new(
             account.address,
             account.network,
             account.token,
             mode,
         ))
-        .await??;
+        .await
+    {
+        Ok(Ok(_)) => {}
+        Ok(Err(e)) => {
+            let err_msg = format!(
+                "Failed to initialize account on driver: {} due to error {}",
+                account.driver, e
+            );
+            log::error!("{}", err_msg);
+            return Err(anyhow!("{}", err_msg));
+        }
+        Err(e) => {
+            let err_msg = format!("Error during GSB call init account - Probably driver {} is not running and receiving messages: {}", account.driver, e);
+            log::error!("{}", err_msg);
+            return Err(anyhow!("{}", err_msg));
+        }
+    }
     log::debug!("Account initialized.");
     Ok(())
 }
