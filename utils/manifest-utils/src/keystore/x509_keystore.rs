7f030d22a0748d7c9a1d0e1db5ce0b80c020112f
scx1332 <sieciech.czajka@golem.network>
2024-02-02 15:42:43 +0100

    f

--- utils/manifest-utils/src/keystore/x509_keystore.rs
+++ utils/manifest-utils/src/keystore/x509_keystore.rs
@@ -361,12 +361,12 @@ impl X509SignatureVerifier {
             let cert_id = cert_to_id(&cert)?;
             cert_ids.push(cert_id);
         }
-        let mut current_cert = self.cert_chain.first().cloned();
+        let current_cert = self.cert_chain.first().cloned();
         if let Some(cert) = current_cert {
             let mut previous_cert = cert;
             while let Some(cert) = issuer(cert_store, &previous_cert) {
                 let cert_id = cert_to_id(&cert)?;
-                cert_ids.push(cert_id);
+                cert_ids.push(cert_id);ar
                 previous_cert = cert;
             }
         }
