83478bb4e3316257e1809162ff24ddd1d7e7a28a
scx1332 <sieciech.czajka@golem.network>
2024-02-15 15:19:13 +0100

    gte

--- extra/payments/multi_test/payment_test.py
+++ extra/payments/multi_test/payment_test.py
@@ -1,7 +1,12 @@
+import os
 import subprocess
 import time
 import json
+import shutil
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 def run_command(command):
     p = subprocess.Popen(command.split(" "), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -10,22 +15,45 @@ def run_command(command):
     return out, err, rc
 
 
-def run_test():
-    subprocess.run(["cargo", "build"], shell=True)
-    subprocess.run(["cargo", "build", "-p", "erc20_processor"], shell=True)
+yagna = None
+processor = None
+
+
+def prepare():
+    logger.info("Preparing test environment...")
 
     with open(".env", "w") as f:
         f.write("RUST_LOG=info,ya_relay_client=error,ya_net=error\n")
-        f.write("YAGNA_DATADIR=data\n")
+        f.write("YAGNA_DATADIR=data_dir\n")
 
-    yagna = "..\\..\\..\\target\\debug\\yagna.exe"
-    processor = "..\\..\\..\\target\\debug\\erc20_processor.exe"
+    if os.path.exists("datadir"):
+        shutil.rmtree("datadir")
+        # operating system need time to propagate events that folder not exists (at least Windows)
+        time.sleep(1)
 
-    pr = subprocess.Popen([yagna, "service", "run"], shell=True)
+    os.mkdir("datadir")
 
-    keys, _, _ = run_command(f"{processor} generate-key -n 10")
+    global yagna
+    global processor
+    yagna = shutil.which("yagna")
+    processor = shutil.which("erc20_processor")
+
+    if yagna is None or processor is None:
+        subprocess.run(["cargo", "build", "-p", "erc20_processor", "-p", "yagna"], shell=True)
+
+        if os.name == "nt":
+            yagna = "..\\..\\..\\target\\debug\\yagna.exe"
+            processor = "..\\..\\..\\target\\debug\\erc20_processor.exe"
+        else:
+            yagna = "../../../target/debug/yagna"
+            processor = "../../../target/debug/erc20_processor"
+
+def create_keys():
+    logger.info("Creating keys...")
+
+    env_file, _, _ = run_command(f"{processor} generate-key -n 10")
     public_addrs = []
-    for line in keys.decode("utf-8").split("\n"):
+    for line in env_file.decode("utf-8").split("\n"):
         print(line)
         if "ETH_ADDRESS_" in line:
             address = line.split(":")[1].strip().lower()
@@ -35,6 +63,11 @@ def run_test():
             print("private keys: ", keys)
             break
 
+    return (env_file, keys, public_addrs)
+
+
+
+def create_account_and_block(eth_private_key, eth_public_key):
     print(keys)
     time.sleep(10)
 
@@ -51,17 +84,47 @@ def run_test():
 
     faucet_address = "0x5b984629E2Cc7570cBa7dD745b83c3dD23Ba6d0f"
 
-    res = run_command(f"{yagna} payment transfer --account {eth_public_key} --amount 1000 --to-address {faucet_address}")
+    res = run_command(f"{yagna} payment transfer --account {eth_public_key} --amount 1100 --to-address {faucet_address}")
     print(res)
 
     time.sleep(80)
 
-    subprocess.run([yagna, "service", "shutdown"], shell=True)
 
-    pr.wait(timeout=30)
+def create_account_and_transfer(eth_private_key, eth_public_key, transfer_to):
+    print(keys)
+    time.sleep(10)
 
+    eth_private_key = keys[0]
+    eth_public_key = public_addrs[0]
+    output, _, _ = run_command(f"{yagna} id create --from-private-key {eth_private_key} --no-password")
+    print(output)
+
+    output, _, _ = run_command(f"{yagna} payment fund --account {eth_public_key} --json")
+
+    output, _, _ = run_command(f"{yagna} id list --json")
+    output_json = json.loads(output)
+    print(output_json)
+
+    faucet_address = "0x5b984629E2Cc7570cBa7dD745b83c3dD23Ba6d0f"
+
+    res = run_command(f"{yagna} payment transfer --account {eth_public_key} --amount 100 --to-address {transfer_to}")
+    print(res)
+
+    time.sleep(80)
 
 
 
 if __name__ == "__main__":
-    run_test()
+    prepare()
+    env_file, keys, public_addrs = create_keys()
+
+
+    pr = subprocess.Popen([yagna, "service", "run"], shell=True)
+
+    create_account_and_transfer(keys[0], public_addrs[0])
+
+    create_account_and_transfer(keys[1], public_addrs[1], public_addrs[0])
+
+    subprocess.run([yagna, "service", "shutdown"], shell=True)
+
+    pr.wait(timeout=60)
