da6b3b3c650734337b9620af7a018f40d043edbb
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2024-04-08 17:43:45 +0200

    allow for streamed and non-streamed response from gsb-http-proxy

--- core/activity/src/http_proxy.rs
+++ core/activity/src/http_proxy.rs
@@ -1,11 +1,16 @@
 use actix_http::Method;
-use actix_web::{web, HttpRequest, HttpResponse};
+use actix_web::http::header;
+use actix_web::web::Bytes;
+use actix_web::{web, Either, HttpRequest, HttpResponse, Responder};
+use futures::{Stream, StreamExt};
 
-use ya_client_model::market::Role;
+use ya_client_model::market::{Agreement, Role};
 use ya_persistence::executor::DbExecutor;
 use ya_service_api_web::middleware::Identity;
+use ya_service_bus::Error;
 
 use crate::common::*;
+use crate::error;
 use ya_core_model::activity;
 use ya_core_model::net::RemoteEndpoint;
 use ya_gsb_http_proxy::http_to_gsb::HttpToGsbProxy;
@@ -22,7 +27,8 @@ async fn get_proxy_http_request(
     path: web::Path<PathActivityUrl>,
     id: Identity,
     request: HttpRequest,
-) -> Result<HttpResponse, actix_web::Error> {
+    // ) -> Result<HttpResponse, actix_web::Error> {
+) -> crate::Result<impl Responder> {
     proxy_http_request(db, path, id, request, None, Method::GET).await
 }
 
@@ -33,7 +39,8 @@ async fn post_proxy_http_request(
     body: web::Bytes,
     id: Identity,
     request: HttpRequest,
-) -> Result<HttpResponse, actix_web::Error> {
+    // ) -> Result<HttpResponse, actix_web::Error> {
+) -> crate::Result<impl Responder> {
     proxy_http_request(db, path, id, request, Some(body), Method::POST).await
 }
 
@@ -44,7 +51,7 @@ async fn proxy_http_request(
     request: HttpRequest,
     body: Option<web::Bytes>,
     method: Method,
-) -> Result<HttpResponse, actix_web::Error> {
+) -> crate::Result<impl Responder> {
     let path_activity_url = path.into_inner();
     let activity_id = path_activity_url.activity_id;
     let path = path_activity_url.url;
@@ -63,6 +70,7 @@ async fn proxy_http_request(
         body: body.map(|bytes| bytes.to_vec()),
         headers: request.headers().clone(),
     };
+
     let stream = http_to_gsb.pass(move |msg| {
         let from = id.identity;
         let to = *agreement.provider_id();
@@ -74,8 +82,32 @@ async fn proxy_http_request(
             .call_streaming(msg)
     });
 
+    if let Some(value) = request.headers().get(header::ACCEPT) {
+        if value.eq(mime::TEXT_EVENT_STREAM.essence_str()) {
+            return Ok(Either::Left(stream_results(stream)?));
+        }
+    }
+    Ok(Either::Right(await_results(stream).await?))
+}
+
+fn stream_results(
+    stream: impl Stream<Item = Result<Bytes, Error>> + Unpin + Sized + Sized,
+) -> crate::Result<impl Responder> {
     Ok(HttpResponse::Ok()
         .keep_alive()
         .content_type(mime::TEXT_EVENT_STREAM.essence_str())
         .streaming(stream))
+}
+
+async fn await_results(
+    mut stream: impl Stream<Item = Result<Bytes, Error>> + Unpin + Sized + Sized,
+) -> crate::Result<impl Responder> {
+    let response = stream.next().await;
+
+    if let Some(Ok(bytes)) = response {
+        let response_body = String::from_utf8(bytes.to_vec())
+            .map_err(|e| error::Error::Service(format!("Conversion from utf8 failed {e}")))?;
+        return Ok(HttpResponse::Ok().body(response_body));
+    }
+    Ok(HttpResponse::InternalServerError().body("No response"))
 }
