094a2a6c22f5163c8e4b088a421ca79fea21b842
scx1332 <sieciech.czajka@golem.network>
2024-07-09 20:25:32 +0200

    Fix release builds (missing openssl vendored feature), fixed issue with git describe not working for dispatched releases.

--- utils/compile-time-utils/build.rs
+++ utils/compile-time-utils/build.rs
@@ -2,6 +2,7 @@ extern crate vergen;
 use vergen::{generate_cargo_keys, ConstantsFlags};
 
 fn main() {
-    let flags = ConstantsFlags::SHA_SHORT | ConstantsFlags::BUILD_DATE;
+    let flags =
+        ConstantsFlags::SHA_SHORT | ConstantsFlags::BUILD_DATE | ConstantsFlags::SEMVER_LIGHTWEIGHT;
     generate_cargo_keys(flags).expect("Unable to generate the cargo keys");
 }
