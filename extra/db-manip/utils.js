2d852211024a5e1cbffa75e198012cec55256f22
scx1332 <sieciech.czajka@golem.network>
2024-08-02 12:53:03 +0200

    working on mockup data for testing

---
+++ extra/db-manip/utils.js
@@ -0,0 +1,13 @@
+
+
+export function formatDatePaymentsFormat(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const milliseconds = String(date.getMilliseconds()).padStart(3, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+}
