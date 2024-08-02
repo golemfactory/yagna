e8f99b030e049ef642b8936540005e27ad8f46bb
scx1332 <sieciech.czajka@golem.network>
2024-08-02 15:22:56 +0200

    wip

--- extra/db-manip/aggreement.js
+++ extra/db-manip/aggreement.js
@@ -1,46 +1,47 @@
-
+import crypto from 'crypto';
 
 
 
 export function insertAgreement(db, agreement) {
     let query = `INSERT INTO pay_agreement (
-                    id,
-                    owner_id,
-                    role,
-                    peer_id,
-                    payee_addr,
-                    payer_addr,
-                    payment_platform,
-                    total_amount_due,
-                    total_amount_accepted,
-                    total_amount_scheduled,
-                    total_amount_paid,
-                    app_session_id,
-                    created_ts,
-                    updated_ts
-                )
-                VALUES (
-                    '${agreement.id}',
-                    '${agreement.owner_id}',
-                    '${agreement.role}',
-                    '${agreement.peer_id}',
-                    '${agreement.payee_addr}',
-                    '${agreement.payer_addr}',
-                    '${agreement.payment_platform}',
-                    '${agreement.total_amount_due}',
-                    '${agreement.total_amount_accepted}',
-                    '${agreement.total_amount_scheduled}',
-                    '${agreement.total_amount_paid}',
-                    '${agreement.app_session_id}',
-                    '${agreement.created_ts}',
-                    '${agreement.updated_ts}'
-                )`;
+        id,
+        owner_id,
+        role,
+        peer_id,
+        payee_addr,
+        payer_addr,
+        payment_platform,
+        total_amount_due,
+        total_amount_accepted,
+        total_amount_scheduled,
+        total_amount_paid,
+        app_session_id,
+        created_ts,
+        updated_ts
+    )
+    VALUES (
+        '${agreement.id}',
+        '${agreement.owner_id}',
+        '${agreement.role}',
+        '${agreement.peer_id}',
+        '${agreement.payee_addr}',
+        '${agreement.payer_addr}',
+        '${agreement.payment_platform}',
+        '${agreement.total_amount_due}',
+        '${agreement.total_amount_accepted}',
+        '${agreement.total_amount_scheduled}',
+        '${agreement.total_amount_paid}',
+        '${agreement.app_session_id}',
+        '${agreement.created_ts}',
+        '${agreement.updated_ts}'
+    )`;
     db.run(query);
 }
 
-export function createAgreement(owner, peer, pay_platform, app_session_id, agreement_id, created_date) {
+
+export function createAgreement(owner, peer, pay_platform, app_session_id, created_date) {
     let agreement = {
-        id: agreement_id,
+        id: crypto.randomBytes(32).toString("hex"),
         owner_id: owner,
         role: 'R',
         peer_id: peer,
@@ -56,4 +57,18 @@ export function createAgreement(owner, peer, pay_platform, app_session_id, agree
         updated_ts: created_date
     }
     return agreement;
+}
+
+export function increaseAgreementAmountDue(db, agreement_id, amount) {
+    let query = `UPDATE pay_agreement 
+                SET total_amount_due = total_amount_due + ${amount}
+                WHERE id = '${agreement_id}'`;
+    db.run(query);
+}
+
+export function increaseAgreementAmountAccepted(db, agreement_id, amount) {
+    let query = `UPDATE pay_agreement 
+                SET total_amount_accepted = total_amount_accepted + ${amount}
+                WHERE id = '${agreement_id}'`;
+    db.run(query);
 }
