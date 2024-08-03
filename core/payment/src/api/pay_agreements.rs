fa560b540c3626065b2bc9c50b6ec2a4e96b26bd
scx1332 <sieciech.czajka@golem.network>
2024-08-03 22:59:02 +0200

    extending API

---
+++ core/payment/src/api/pay_agreements.rs
@@ -0,0 +1,45 @@
+// External crates
+use actix_web::web::{get, Data, Path};
+use actix_web::{HttpResponse, Scope};
+use ya_persistence::executor::DbExecutor;
+use ya_service_api_web::middleware::Identity;
+use crate::dao::*;
+use crate::utils::*;
+
+pub fn register_endpoints(scope: Scope) -> Scope {
+    scope
+        .route("/payAgreements", get().to(get_pay_agreements))
+        .route("/payAgreement/{agreement_id}", get().to(get_pay_agreement))
+}
+
+async fn get_pay_agreements(
+    db: Data<DbExecutor>,
+    id: Identity,
+) -> HttpResponse {
+    let node_id = id.identity;
+    let dao: AgreementDao = db.as_dao();
+    match dao
+        .list(None)
+        .await
+    {
+        Ok(agreements) => response::ok(agreements),
+        Err(e) => response::server_error(&e),
+    }
+}
+
+async fn get_pay_agreement(
+    db: Data<DbExecutor>,
+    path: Path<String>,
+    id: Identity,
+) -> HttpResponse {
+    let node_id = id.identity;
+    let agreement_id = path.into_inner();
+    let dao: AgreementDao = db.as_dao();
+    match dao
+        .get(agreement_id, node_id)
+        .await
+    {
+        Ok(agreement) => response::ok(agreement),
+        Err(e) => response::server_error(&e),
+    }
+}
