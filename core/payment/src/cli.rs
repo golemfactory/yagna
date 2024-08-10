c5e7135cf91b93bcdd1615d30fbe71f8ed1aa79a
scx1332 <sieciech.czajka@golem.network>
2024-08-10 13:57:51 +0200

    working on resolving invoices

--- core/payment/src/cli.rs
+++ core/payment/src/cli.rs
@@ -441,7 +441,7 @@ Typically operation should take less than 1 minute.
                                     account.driver(),
                                     account.network(),
                                     account.token()
-                                ),
+                                ).to_lowercase(),
                                 skip_resolve: false,
                                 skip_send: false,
                             })
