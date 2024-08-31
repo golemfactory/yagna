0b1ee34e4b1727e3983d94bc07b1a1f8209a3ffd
scx1332 <sieciech.czajka@golem.network>
2024-08-31 22:26:53 +0200

    migrations cannot be applied

--- core/payment/migrations/2024-08-12-152257_timestamps/down.sql
+++ core/payment/migrations/2024-08-12-152257_timestamps/down.sql
@@ -1,1 +1,1 @@
--- This file should undo anything in `up.sql`
+-- It is not possible to revert this migration.
--- core/payment/migrations/2024-08-12-171012_extended_order/down.sql
+++ core/payment/migrations/2024-08-12-171012_extended_order/down.sql
@@ -1,7 +1,2 @@
--- This file should undo anything in `up.sql`
-DROP TABLE PAY_BATCH_ORDER_ITEM_PAYMENT;
-
-DROP TABLE PAY_BATCH_ORDER_ITEM;
-
-DROP TABLE PAY_BATCH_ORDER;
+-- It is not possible to revert this migration.
 
