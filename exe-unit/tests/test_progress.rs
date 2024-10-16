8b7b170131adfc37ed713a77741d60c1f3384c38
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2024-10-16 11:36:47 +0200

    ExeUnit tests should be behind feature flag

--- exe-unit/tests/test_progress.rs
+++ exe-unit/tests/test_progress.rs
@@ -17,6 +17,7 @@ use ya_service_bus::typed as bus;
 
 /// Test if progress reporting mechanisms work on gsb level
 /// with full ExeUnit setup.
+#[cfg_attr(not(feature = "framework-test"), ignore)]
 #[test_context(DroppableTestContext)]
 #[serial_test::serial]
 async fn test_progress_reporting(ctx: &mut DroppableTestContext) -> anyhow::Result<()> {
