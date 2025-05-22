72f72665ea9726b6eaf8d7b0b141a81ec2d548ca
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2025-05-22 14:26:48 +0200

    Use clap in market config

--- core/market/src/config.rs
+++ core/market/src/config.rs
@@ -1,59 +1,59 @@
+use clap::Parser;
 use std::time::Duration;
-use structopt::StructOpt;
 use url::Url;
 
-#[derive(StructOpt, Clone)]
+#[derive(Parser, Clone)]
 pub struct Config {
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub discovery: DiscoveryConfig,
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub subscription: SubscriptionConfig,
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub events: EventsConfig,
-    #[structopt(flatten)]
+    #[clap(flatten)]
     pub db: DbConfig,
 }
 
-#[derive(StructOpt, Clone, Debug)]
+#[derive(Parser, Clone, Debug)]
 pub struct DiscoveryConfig {
-    #[structopt(env, parse(try_from_str = parse_url), default_value = "http://localhost:8545")]
+    #[clap(env, value_parser = parse_url, default_value = "http://localhost:8545")]
     pub golem_base_rpc_url: Url,
-    #[structopt(env, parse(try_from_str = parse_url), default_value = "ws://localhost:8545")]
+    #[clap(env, value_parser = parse_url, default_value = "ws://localhost:8545")]
     pub golem_base_ws_url: Url,
 }
 
-#[derive(StructOpt, Clone)]
+#[derive(Parser, Clone)]
 pub struct SubscriptionConfig {
-    #[structopt(env = "DEFAULT_SUBSCRIPTION_TTL", parse(try_from_str = parse_chrono_duration), default_value = "1h")]
+    #[clap(env = "DEFAULT_SUBSCRIPTION_TTL", value_parser = parse_chrono_duration, default_value = "1h")]
     pub default_ttl: chrono::Duration,
 }
 
-#[derive(StructOpt, Clone)]
+#[derive(Parser, Clone)]
 pub struct EventsConfig {
-    #[structopt(env = "MARKET_MAX_EVENTS_DEFAULT", default_value = "20")]
+    #[clap(env = "MARKET_MAX_EVENTS_DEFAULT", default_value = "20")]
     pub max_events_default: i32,
-    #[structopt(env = "MARKET_MAX_EVENTS_MAX", default_value = "100")]
+    #[clap(env = "MARKET_MAX_EVENTS_MAX", default_value = "100")]
     pub max_events_max: i32,
 }
 
-#[derive(StructOpt, Clone)]
+#[derive(Parser, Clone)]
 pub struct DbConfig {
     /// Interval in which Market cleaner will be invoked
-    #[structopt(env = "MARKET_DB_CLEANUP_INTERVAL", parse(try_from_str = humantime::parse_duration), default_value = "4h")]
+    #[clap(env = "MARKET_DB_CLEANUP_INTERVAL", value_parser = humantime::parse_duration, default_value = "4h")]
     pub cleanup_interval: Duration,
     /// Number of days to persist Agreements and related Agreement Events
-    #[structopt(env = "MARKET_AGREEMENT_STORE_DAYS", default_value = "90")]
+    #[clap(env = "MARKET_AGREEMENT_STORE_DAYS", default_value = "90")]
     pub agreement_store_days: i32,
     /// Number of days to persist Negotiation Events
-    #[structopt(env = "MARKET_EVENT_STORE_DAYS", default_value = "1")]
+    #[clap(env = "MARKET_EVENT_STORE_DAYS", default_value = "1")]
     pub event_store_days: i32,
 }
 
 impl Config {
-    pub fn from_env() -> Result<Config, structopt::clap::Error> {
+    pub fn from_env() -> Result<Config, clap::Error> {
         // Empty command line arguments, because we want to use ENV fallback
         // or default values if ENV variables are not set.
-        Config::from_iter_safe(&[""])
+        Config::try_parse_from(&[""])
     }
 }
 
@@ -70,20 +70,20 @@ mod test {
     use super::Config;
 
     #[test]
-    fn test_default_structopt_subscription_ttl() {
+    fn test_default_clap_subscription_ttl() {
         let c = Config::from_env().unwrap();
         assert_eq!(60, c.subscription.default_ttl.num_minutes());
     }
 
     #[test]
-    fn test_default_structopt_events() {
+    fn test_default_clap_events() {
         let c = Config::from_env().unwrap();
         assert_eq!(20, c.events.max_events_default);
         assert_eq!(100, c.events.max_events_max);
     }
 
     #[test]
-    fn test_default_structopt_db_config() {
+    fn test_default_clap_db_config() {
         let c = Config::from_env().unwrap();
         assert_eq!(4 * 3600, c.db.cleanup_interval.as_secs());
         assert_eq!(90, c.db.agreement_store_days);
