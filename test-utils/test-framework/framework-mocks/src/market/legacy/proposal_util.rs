facc089156a340cde4aae313123ba8f630f6c075
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2025-07-25 14:25:38 +0200

    Check on CI if we are able to publish offer from different id

--- test-utils/test-framework/framework-mocks/src/market/legacy/proposal_util.rs
+++ test-utils/test-framework/framework-mocks/src/market/legacy/proposal_util.rs
@@ -95,8 +95,8 @@ pub async fn exchange_draft_proposals(
     {
         Ok(helper) => Ok(helper),
         Err(e) if e.to_string().contains("Timeout") => {
-            let prov_mkt = network.get_market(prov_name);
-            let prov_id = network.get_default_id(prov_name).await;
+            let prov_mkt = network.get_market(req_name);
+            let prov_id = network.get_default_id(req_name).await;
             prov_mkt
                 .subscribe_offer(&exclusive_offer("test-match-on"), &prov_id)
                 .await
