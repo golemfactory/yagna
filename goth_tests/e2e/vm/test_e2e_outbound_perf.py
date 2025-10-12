db9564d84a85f709fccdef4790a2dfdea5c38faa
scx1332 <sieciech@gmail.com>
2025-10-12 17:07:00 +0200

    test anything

--- goth_tests/e2e/vm/test_e2e_outbound_perf.py
+++ goth_tests/e2e/vm/test_e2e_outbound_perf.py
@@ -52,7 +52,7 @@ def vm_exe_script(runner: Runner, addr: str, output_file: str):
     return [
         {"deploy": {}},
         {"start": {}},
-        {"run": {"entry_point": exe, "args": command.split(" ")}},
+        {"run": {"entry_point": "/bin/ls", "args": []}},
     ]
 
 
